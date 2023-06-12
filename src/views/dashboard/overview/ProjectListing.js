6eaafa0422f6c82df35e219325aae0957befbc76
Nilesh <nilesh.dangi@wowlabz.com>
2023-06-12 17:17:04 +0530

    TD-79 - Upcoming payments accordian item added for client Implemented: Added upcoming payments accordian item for client user type and removed recommended projects item Because: conditionally rendering items for client and talent user type

--- src/views/dashboard/overview/ProjectListing.js
+++ src/views/dashboard/overview/ProjectListing.js
@@ -1,11 +1,13 @@
 /* eslint-disable react/require-default-props */
 import { useState } from 'react';
 import Proptypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import { Accordion, AccordionBody, AccordionHeader, AccordionItem, Card, CardBody, CardText } from 'reactstrap';
 
 import ActiveProjectsEmptyGif from '@src/assets/images/GetStarted.gif';
 import UpcomingProjectsEmptyGif from '@src/assets/images/emptyGif.gif';
+import PaymentsEmptyGif from '@src/assets/images/no-payments.gif';
 import Project from './Project';
 import { ProjectWrapper, ProjectsListingWrap } from './style';
 
@@ -13,18 +15,25 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { useIsTab } from '../../../utility/Utils';
 import Tag from '../../../@core/components/tags';
+import { userData } from '../../../redux/selectors/dashboardSelectors';
 
-const Empty = ({ active }) => (
+const Empty = ({ active, recommended, payment }) => (
   <ProjectWrapper>
     <Card className="empty-card">
       <CardBody className="empty empty-h-25">
-        <img src={active ? ActiveProjectsEmptyGif : UpcomingProjectsEmptyGif} className="empty-gif" alt="empty-gif" />
+        {active && <img src={ActiveProjectsEmptyGif} className="empty-gif" alt="empty-gif" />}
+        {recommended && <img src={UpcomingProjectsEmptyGif} className="empty-gif" alt="empty-gif" />}
+        {payment && <img src={PaymentsEmptyGif} className="empty-gif" alt="empty-gif" />}
         {active && (
           <CardText className="get-started">
             Lets get you <br /> started!
           </CardText>
         )}
-        <div className="font-weight-normal text-center text-primary project-cta mt-25">Explore Project</div>
+        {payment && <CardText className="get-started">No Upcoming Payment</CardText>}
+        {active ||
+          (recommended && (
+            <div className="font-weight-normal text-center text-primary project-cta mt-25">Explore Project</div>
+          ))}
       </CardBody>
     </Card>
   </ProjectWrapper>
@@ -84,6 +93,8 @@ const ProjectListing = () => {
     arrows: true,
   };
 
+  const userDetailsData = useSelector(userData);
+
   return (
     <Accordion className="accordion-margin" open={open} toggle={toggle}>
       <AccordionItem>
@@ -91,7 +102,13 @@ const ProjectListing = () => {
           Active Projects <Tag>0</Tag>
         </AccordionHeader>
         <AccordionBody accordionId="1">
-          <ProjectsListingWrap>{isTab ? <Empty active /> : <Empty active />}</ProjectsListingWrap>
+          <ProjectsListingWrap>
+            {isTab ? (
+              <Empty active recommended={false} payment={false} />
+            ) : (
+              <Empty active recommended={false} payment={false} />
+            )}
+          </ProjectsListingWrap>
         </AccordionBody>
       </AccordionItem>
       <AccordionItem>
@@ -99,30 +116,56 @@ const ProjectListing = () => {
           Upcoming Projects <Tag hasNew>0 new</Tag>
         </AccordionHeader>
         <AccordionBody accordionId="2">
-          <ProjectsListingWrap>{isTab ? <Empty active={false} /> : <Empty active={false} />}</ProjectsListingWrap>
-        </AccordionBody>
-      </AccordionItem>
-      <AccordionItem>
-        <AccordionHeader targetId="3">
-          Recommended Projects <Tag hasNew>2 new</Tag>
-        </AccordionHeader>
-        <AccordionBody accordionId="3">
           <ProjectsListingWrap>
             {isTab ? (
-              <>
-                {ProjectsArray.map((project) => (
-                  <Project key={project.id} data={project} recommended />
-                ))}
-              </>
+              <Empty active={false} recommended payment={false} />
             ) : (
-              <Slider {...settings}>
-                {ProjectsArray.map((project, index) => (
-                  <Project className={`slide-${index}`} key={project.id} data={project} recommended />
-                ))}
-              </Slider>
+              <Empty active={false} recommended payment={false} />
             )}
           </ProjectsListingWrap>
         </AccordionBody>
+      </AccordionItem>
+      <AccordionItem>
+        {userDetailsData?.user_type === 'TALENT' && (
+          <>
+            <AccordionHeader targetId="3">
+              Recommended Projects <Tag hasNew>2 new</Tag>
+            </AccordionHeader>
+            <AccordionBody accordionId="3">
+              <ProjectsListingWrap>
+                {isTab ? (
+                  <>
+                    {ProjectsArray.map((project) => (
+                      <Project key={project.id} data={project} recommended />
+                    ))}
+                  </>
+                ) : (
+                  <Slider {...settings}>
+                    {ProjectsArray.map((project, index) => (
+                      <Project className={`slide-${index}`} key={project.id} data={project} recommended />
+                    ))}
+                  </Slider>
+                )}
+              </ProjectsListingWrap>
+            </AccordionBody>
+          </>
+        )}
+        {userDetailsData?.user_type === 'CLIENT' && (
+          <>
+            <AccordionHeader targetId="3">
+              Upcoming Payments <Tag hasNew>0 new</Tag>
+            </AccordionHeader>
+            <AccordionBody accordionId="3">
+              <ProjectsListingWrap>
+                {isTab ? (
+                  <Empty active={false} recommended={false} payment />
+                ) : (
+                  <Empty active={false} recommended={false} payment />
+                )}
+              </ProjectsListingWrap>
+            </AccordionBody>
+          </>
+        )}
       </AccordionItem>
     </Accordion>
   );
@@ -132,8 +175,12 @@ export default ProjectListing;
 
 Empty.propTypes = {
   active: Proptypes.bool,
+  recommended: Proptypes.bool,
+  payment: Proptypes.bool,
 };
 
 Empty.defaultProps = {
   active: false,
+  recommended: false,
+  payment: false,
 };
