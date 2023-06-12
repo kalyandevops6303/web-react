4bc7b9ecc9dea7f6fcb9862c58a1e3c35390356f
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-06-12 07:29:37 +0530

    Feat: TD-72 API setup for profile details page

---
+++ src/services/profileServices.js
@@ -0,0 +1,23 @@
+import API from '../configs/api';
+import DataService from '../configs/dataService/dataService';
+
+const getTalentService = (data) => DataService.get(`${API.profile.talent}/${data}`);
+
+const getTalentProjectService = (data) => DataService.get(API.profile.talentProjects, data);
+
+const getClientService = (data) => DataService.get(`${API.profile.client}/${data}`);
+
+const getClientProjectService = (data) => DataService.get(API.profile.clientProjects, data);
+
+const makeFavService = (id, user_type) => DataService.post(API.profile.addToFav, { user_id: id, user_type });
+
+const removeFavService = (id, user_type) => DataService.post(API.profile.removeFav, { user_id: id, user_type });
+
+export {
+  getTalentService,
+  getTalentProjectService,
+  getClientService,
+  getClientProjectService,
+  makeFavService,
+  removeFavService,
+};
