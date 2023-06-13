768050001d6f42c98da5f061df17bd808079802a
Nilesh <nilesh.dangi@wowlabz.com>
2023-06-13 08:39:28 +0530

    TD-79 - Showed data from redux in header user details Implemented: Showed data coming from redux instead of local storage in header user details Because: Delay in data fetching from local storage

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -12,32 +12,32 @@ import { UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem } from
 
 // ** Default Avatar Image
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg';
-import { getItem } from '../../../../utility/localStorageControl';
+import { useSelector } from 'react-redux';
+import { userData } from '../../../../redux/selectors/dashboardSelectors';
 
 const UserDropdown = () => {
-  const userData = getItem('userData');
+  const userDetailsData = useSelector(userData);
 
   const handleLogout = () => {
     localStorage.clear();
   };
-  console.log(userData?.talent_info?.first_name);
   return (
     <UncontrolledDropdown tag="li" className="dropdown-user nav-item">
       <DropdownToggle href="/" tag="a" className="nav-link dropdown-user-link" onClick={(e) => e.preventDefault()}>
         <div className="user-nav d-sm-flex d-none">
           <span className="user-name fw-bold">
-            {userData?.user_type === 'TALENT'
-              ? userData?.talent_info?.first_name || 'User'
-              : userData?.client_info?.first_name || 'User'}
+            {userDetailsData?.user_type === 'TALENT'
+              ? userDetailsData?.talent_info?.first_name || 'User'
+              : userDetailsData?.client_info?.first_name || 'User'}
           </span>
-          <span className="user-status">{userData?.user_type || 'Role'}</span>
+          <span className="user-status">{userDetailsData?.user_type || 'Role'}</span>
         </div>
         <Avatar img={defaultAvatar} imgHeight="40" imgWidth="40" status="online" />
       </DropdownToggle>
       <DropdownMenu end>
         <DropdownItem
           tag={Link}
-          to={`/profile/${userData?.user_type}/${userData?._id}`}
+          to={`/profile/${userDetailsData?.user_type}/${userDetailsData?._id}`}
           // onClick={(e) => e.preventDefault()}
         >
           <User size={14} className="me-75" />
