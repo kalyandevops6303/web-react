33ab467dd45204559ca5bc732d9a897973afed7f
Nilesh <nilesh.dangi@wowlabz.com>
2023-06-26 19:02:23 +0530

    TD-204 - Added notifications route and index file Implemented: Added new notifications route with its index file

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -33,6 +33,7 @@ const AuthRoute = lazy(() => import('../../views/auth/index'));
 const TalentOnboarding = lazy(() => import('../../views/Onboarding/Talent'));
 const ClientOnboarding = lazy(() => import('../../views/Onboarding/Client'));
 const CreateProject = lazy(() => import('../../views/CreateProject'));
+const Notifications = lazy(() => import('../../views/notifications'));
 
 // ** Merge Routes
 const Routes = [
@@ -94,6 +95,10 @@ const Routes = [
   {
     path: '/create-project',
     element: <CreateProject />,
+  },
+  {
+    path: '/notifications',
+    element: <Notifications />,
   },
 ];
 
