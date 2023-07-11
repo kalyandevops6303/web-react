8d837c5850dfa289f0b6612b1f8df96768022fe2
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-07-11 19:00:29 +0530

    Fix: TD-574 Country list updated with ID

--- src/views/auth/ForgotPassword.js
+++ src/views/auth/ForgotPassword.js
@@ -85,7 +85,7 @@ const RegisterEmail = () => {
           </div>
 
           <Button color="primary" block className="auth-btn" type="submit" disabled={!emailValue || isLoading}>
-            {isLoading ? <Spinner size="sm" /> : 'Send OTP'}
+            {isLoading ? <Spinner size="sm" /> : 'Sumbit'}
           </Button>
         </Form>
         <div className="divider my-2">
