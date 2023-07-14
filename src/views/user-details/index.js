210572aaf35b97d6f8754b0400203abf1ff3c498
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-07-14 12:47:47 +0530

    Fix: TD-588 Public, private dashboard and search page ui handled

--- src/views/user-details/index.js
+++ src/views/user-details/index.js
@@ -106,7 +106,7 @@ const UserDetails = () => {
             {!isClient && (
               <Col lg="3">
                 <Statbox
-                  title={`${currentProfile?.currency_preference?.code} ${currentProfile?.hourly_rate}`}
+                  title={`${currentProfile?.currency_preference?.code || ''} ${currentProfile?.hourly_rate || 0}`}
                   desc="Hourly Rate"
                   icon={<img src={MoneyIcon} height={22} alt="money" />}
                   color="light-warning"
@@ -130,8 +130,9 @@ const UserDetails = () => {
                     {calculateAvailableHoursPerWeek(currentProfile?.availability) < 0
                       ? 0
                       : round(calculateAvailableHoursPerWeek(currentProfile?.availability), 2)}{' '}
-                    hours/week <br /> {currentProfile?.availability?.timezone?.abbreviation} (
-                    {currentProfile?.availability?.timezone?.offset_name})
+                    hours/week <br />
+                    {currentProfile?.availability?.timezone?.abbreviation}(
+                    {currentProfile?.availability?.timezone?.offset_name || 'Time zone'})
                   </>
                 }
                 desc="Availability"
