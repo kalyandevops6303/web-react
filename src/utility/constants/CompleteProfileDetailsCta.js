1833c57d8fdebf24a32eba285424adfbb9c62a8a
Nilesh <nilesh.dangi@wowlabz.com>
2023-07-19 14:49:46 +0530

    TD-610 - Added complete profile cta and functionality Implemented: Added functionality of showing the cta of add details on private dashboard and redirecting the user to particular section for adding those details

---
+++ src/utility/constants/CompleteProfileDetailsCta.js
@@ -0,0 +1,46 @@
+const CompleteProfileDetailsCta = {
+  TALENT: [
+    {
+      keyToMatch: 'tagline',
+      label: 'Add Personal Details',
+      path: '/talent-onboarding/personal-details',
+    },
+    {
+      keyToMatch: 'educational_institute',
+      label: 'Add Education Details',
+      path: '/talent-onboarding/educational-details',
+    },
+    { keyToMatch: 'availability', label: 'Add Availability Details', path: '/talent-onboarding/availability-details' },
+    { keyToMatch: 'social_links', label: 'Add Social Details', path: '/talent-onboarding/social-details' },
+  ],
+  CLIENT: [
+    { keyToMatch: 'company_name', label: 'Add Personal Details', path: '/client-onboarding/personal-details' },
+    {
+      keyToMatch: 'educational_institute',
+      label: 'Add Education Details',
+      path: '/client-onboarding/educational-details',
+    },
+    { keyToMatch: 'availability', label: 'Add Availability Details', path: '/client-onboarding/availability-details' },
+    { keyToMatch: 'social_links', label: 'Add Social Details', path: '/client-onboarding/social-details' },
+  ],
+};
+
+// eslint-disable-next-line consistent-return
+const returnCompleteProfileDetailsCta = (userType, missingValues) => {
+  if (userType === 'CLIENT' && missingValues?.includes('company_name')) {
+    return CompleteProfileDetailsCta[userType].find((item) => item.keyToMatch === 'company_name');
+    // eslint-disable-next-line no-else-return
+  } else if (userType === 'TALENT' && missingValues?.includes('tagline')) {
+    return CompleteProfileDetailsCta[userType].find((item) => item.keyToMatch === 'tagline');
+  } else if (missingValues?.includes('educational_institute')) {
+    return CompleteProfileDetailsCta[userType].find((item) => item.keyToMatch === 'educational_institute');
+  } else if (missingValues?.includes('availability')) {
+    return CompleteProfileDetailsCta[userType].find((item) => item.keyToMatch === 'availability');
+  } else if (missingValues?.includes('social_links')) {
+    return CompleteProfileDetailsCta[userType].find((item) => item.keyToMatch === 'social_links');
+  } else {
+    return null;
+  }
+};
+
+export default returnCompleteProfileDetailsCta;
