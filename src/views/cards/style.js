d9e690209ab00f6777ddd502be9f1254fd642f40
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-07-27 17:43:08 +0530

    Revert "feat: Truncate change"
    This reverts commit dc2043d4eda5d529b2a36a78ae8fe2e3a73a1d03.

--- src/views/cards/style.js
+++ src/views/cards/style.js
@@ -145,7 +145,6 @@ const UserCardWrap = styled(CardWrap)`
     text-overflow: ellipsis;
     white-space: nowrap;
   }
-  .truncate-4 {
     display: -webkit-box;
     -webkit-line-clamp: 4;
     -webkit-box-orient: vertical;
