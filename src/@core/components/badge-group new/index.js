b7bf81a6e44d5e4e6a1f224e40bef38bb7ef833a
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-07-27 14:59:20 +0530

    Feat: Cards updated based on discussion

---
+++ src/@core/components/badge-group new/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Badge, UncontrolledTooltip } from 'reactstrap';
+import BadgeGroupWrap from './style';
+import { CustomBadge } from '../../../views/styled';
+
+const BadgeGroup = ({ title, data, color }) => {
+  if (!data || data.length === 0) {
+    return null;
+  }
+  if (data?.name) {
+    return (
+      <BadgeGroupWrap>
+        <div className="badge-box-wrap mb-50">
+          <div className="info-key">{title || ''}</div>
+          <div className="badge-box mt-25">
+            <CustomBadge>
+              <Badge className={color} color={color}>
+                {data?.name}
+              </Badge>
+            </CustomBadge>
+          </div>
+        </div>
+      </BadgeGroupWrap>
+    );
+  }
+
+  const renderBadge = (item, index) => {
+    const { name } = item;
+    const isLongName = name.length > 35;
+    const badgeClassName = isLongName ? `${color} truncate-1` : color;
+    const badgeColor = `${color} badge`;
+
+    return (
+      <span key={index}>
+        {isLongName ? (
+          <>
+            <CustomBadge>
+              <Badge className={badgeClassName} color={badgeColor} id={`tooltip-${index}`}>
+                {name}
+              </Badge>
+            </CustomBadge>
+            <UncontrolledTooltip target={`tooltip-${index}`}>{name}</UncontrolledTooltip>
+          </>
+        ) : (
+          <CustomBadge>
+            <Badge className={badgeClassName} color={badgeColor}>
+              {name}
+            </Badge>
+          </CustomBadge>
+        )}
+      </span>
+    );
+  };
+
+  return (
+    <BadgeGroupWrap>
+      <div className="badge-box-wrap mb-50">
+        <div className="info-key">{title || ''}</div>
+        <div className="badge-box mt-25">{data && data?.map(renderBadge)}</div>
+      </div>
+    </BadgeGroupWrap>
+  );
+};
+
+export default BadgeGroup;
