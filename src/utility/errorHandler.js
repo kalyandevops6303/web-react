71354714d66e8c0562351a82235134a6a735f2de
Nilesh <nilesh.dangi@wowlabz.com>
2023-07-27 11:49:39 +0530

    feat - added error for connection issues

--- src/utility/errorHandler.js
+++ src/utility/errorHandler.js
@@ -41,6 +41,8 @@ const handleErrorCode = (err, callBack) => {
     } else {
       handleError(err, callBack);
     }
+  } else {
+    showErrorNotification('Please check your connection!');
   }
 };
 
@@ -59,8 +61,6 @@ const errorHandler = (err, callBack) => {
     } else {
       handleErrorCode(err, callBack);
     }
-  } else {
-    showErrorNotification('Please check your connection!');
   }
 };
 export default errorHandler;
