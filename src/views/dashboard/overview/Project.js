d71d8db63ecd7eb9c7a53b43d5f79927af94c38f
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-07-31 10:12:40 +0530

    Feat: Dashboard recommendation cards UI updated

--- src/views/dashboard/overview/Project.js
+++ src/views/dashboard/overview/Project.js
@@ -18,6 +18,7 @@ import { ProjectWrapper } from './style';
 import theme from '../../../configs/themeVariables';
 import ProjectModal from '../../modals/ProjectModal';
 import { CustomBadge } from '../../styled';
+import TagsSection from './TagsSection';
 
 const UserSection = ({ users, tagName, name, isAlma }) => (
   <div className="user-section">
@@ -51,34 +52,7 @@ UserSection.propTypes = {
   tagName: PropTypes.string,
 };
 
-const TagsSection = ({ tags }) => (
-  <div className="tags-container">
-    {tags.length > 4 ? (
-      <>
-        {tags.slice(0, 4).map((tag) => (
-          <Badge key={tag.name} className="tag-margin">
-            {tag.name}
-          </Badge>
-        ))}
-        <span className="additional-text">+3</span>
-      </>
-    ) : (
-      <>
-        {tags.slice(0, 4).map((tag) => (
-          <Badge key={tag.name} className="tag-margin">
-            {tag.name}
-          </Badge>
-        ))}
-      </>
-    )}
-  </div>
-);
-
-TagsSection.propTypes = {
-  tags: PropTypes.array,
-};
-
-const Project = ({ data, className, recommended }) => {
+const Project = ({ open, data, className, recommended }) => {
   const [showModal, setShowModal] = useState(false);
 
   const handleToggle = () => {
@@ -190,7 +164,7 @@ const Project = ({ data, className, recommended }) => {
                 </div>
               </CircularProgressbarWithChildren>
             </div>
-            <TagsSection tags={data?.proficiency.skills} />
+            <TagsSection open={open} tags={data?.proficiency.skills} />
           </div>
           <div className="main-row">
             <UserSection
@@ -239,5 +213,6 @@ Project.propTypes = {
   data: PropTypes.object,
   className: PropTypes.string,
   recommended: PropTypes.bool,
+  open: PropTypes.string,
 };
 export default Project;
