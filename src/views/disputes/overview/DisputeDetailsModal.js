b3d4de3713e4c07c02f7fd74910e2bc9ceae21e8
Nilesh <nilesh.dangi@wowlabz.com>
2023-09-01 11:05:11 +0530

    TD-699 - Dispute details modal ui completed Implemented: Added feature of adding a new reply, also added upload button for the response

--- src/views/disputes/overview/DisputeDetailsModal.js
+++ src/views/disputes/overview/DisputeDetailsModal.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Proptypes from 'prop-types';
 import '../../custom-styles.scss';
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import * as yup from 'yup';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Modal, ModalHeader, ModalBody, Form, Row, Col, Input, Button } from 'reactstrap';
+import { Upload } from 'react-feather';
 import Avatar from '@components/avatar';
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg';
 import { DisputeDetailsContainer } from '../style';
 import theme from '../../../configs/themeVariables';
 import Timeline from '../../../@core/components/timeline';
+import ShowToastMessage from '../../../@core/components/toast';
+import { ERROR } from '../../../utility/constants/ToastTypes';
 
 const DisputeDetailsModal = ({ modal, toggleModal }) => {
   const timelineData = [
@@ -61,7 +67,7 @@ const DisputeDetailsModal = ({ modal, toggleModal }) => {
       customContent: (
         <div>
           <div className="d-flex justify-content-between mb-25">
-            <p className="fw-bold mb-0">Dispute Response</p>
+            <p className="fw-bold mb-0">Dispute Raise</p>
             <p className="font-small-3 mb-0">2 Day ago</p>
           </div>
           <p>Apr 28, 23</p>
@@ -81,6 +87,86 @@ const DisputeDetailsModal = ({ modal, toggleModal }) => {
       ),
     },
   ];
+
+  const ResponseSchema = yup.object().shape({
+    response: yup
+      .string()
+      .min(100, 'Response must be at least 100 characters')
+      .max(2000, 'Response must be 2000 characters or less')
+      .required('Response is required'),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(ResponseSchema),
+  });
+
+  const [updatedTimelineData, setUpdatedTimelineData] = useState(timelineData);
+  const [isReplyBoxPresent, setIsReplyBoxPresent] = useState(false);
+
+  const onSubmit = () => {
+    trigger();
+    if (errors) {
+      ShowToastMessage(ERROR, 'Please enter your response to dispute before submitting');
+    }
+  };
+
+  const onCancelClick = () => {
+    setUpdatedTimelineData(updatedTimelineData);
+    setIsReplyBoxPresent(false);
+  };
+
+  const onReplyClick = () => {
+    const newReply = {
+      isDisabled: false,
+      color: theme.orangeColor,
+      customContent: (
+        <div>
+          <div className="mb-25">
+            <p className="fw-bold mb-0">Response Dispute</p>
+          </div>
+          <div>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Row className="mb-1">
+                <Col sm="12" md="12" lg="6">
+                  <Controller
+                    id="response"
+                    name="response"
+                    control={control}
+                    render={({ field }) => (
+                      <Input {...field} type="textarea" placeholder="Enter your respone to dispute" rows="3" />
+                    )}
+                  />
+                </Col>
+              </Row>
+              <Button color="primary" className="mt-2 d-flex align-items-center py-50">
+                <Upload size={18} className="me-75" />
+                Upload
+              </Button>
+              <div className="d-flex justify-content-end">
+                <Button outline color="primary" className="me-3" onClick={onCancelClick}>
+                  Cancel
+                </Button>
+                <Button color="primary" onClick={onSubmit}>
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      ),
+    };
+
+    if (!isReplyBoxPresent) {
+      setUpdatedTimelineData([newReply, ...updatedTimelineData]);
+      setIsReplyBoxPresent(true);
+    }
+  };
 
   return (
     <Modal isOpen={modal} contentClassName="listing-team-members-modal-style" className="modal-dialog-centered">
@@ -105,9 +191,11 @@ const DisputeDetailsModal = ({ modal, toggleModal }) => {
           </div>
           <div className="d-flex justify-content-end align-items-center my-2">
             <p className="text-decoration-underline fw-bold blue-btn mb-0 me-3 cursor-pointer">Dispute Resolved</p>
-            <p className="text-decoration-underline fw-bold blue-btn mb-0 cursor-pointer">Reply</p>
+            <p className="text-decoration-underline fw-bold blue-btn mb-0 cursor-pointer" onClick={onReplyClick}>
+              Reply
+            </p>
           </div>
-          <Timeline data={timelineData} />
+          <Timeline data={updatedTimelineData} />
         </DisputeDetailsContainer>
       </ModalBody>
     </Modal>
