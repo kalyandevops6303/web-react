ad1be28af1af6a647edf1a7fecaa8cc379bf36ef
Aditya Puri <adityapuri85@gmail.com>
2023-09-11 21:22:36 +0530

    fix:tags overflow fix for leftsidebar

--- src/@core/components/badge-group-dynamic-count/style.js
+++ src/@core/components/badge-group-dynamic-count/style.js
@@ -27,7 +27,7 @@ export const BadgeGroupWrap = styled.div`
       .badge {
         margin: 0 0.5rem 0.5rem 0;
         font-size: 0.75rem;
-        max-width: 14rem;
+        max-width: 97%;
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
