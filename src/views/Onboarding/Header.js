2cdd0b07604c201a6645117a001b58210f1fb272
Aditya <aditya.puri@wowlabz.com>
2023-09-14 18:39:53 +0530

    onboarding header logo updated

--- src/views/Onboarding/Header.js
+++ src/views/Onboarding/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { HeaderContainer } from './style';
-import Logo from '../../assets/images/trumio-logo-onboarding-header.png';
+import Logo from '../../assets/images/ic_trumio_logo.png';
 
 const Header = () => (
   <HeaderContainer>
     <div className="logo-wrap">
-      <img alt="logo" src={Logo} className="card-logo" />
+      <img alt="logo" src={Logo} className="card-logo onboarding-header-logo" />
       <span className="ms-25 version">v0.0.5</span>
     </div>
   </HeaderContainer>
