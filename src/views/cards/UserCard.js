bf7eebf450121547ae53bbf3eeda013f1e7bc268
jagannath kv <jagannath.kv@wowlabz.com>
2023-09-15 12:00:17 +0530

    fixed ui issue

--- src/views/cards/UserCard.js
+++ src/views/cards/UserCard.js
@@ -5,7 +5,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { Heart, MapPin } from 'react-feather';
 import { useDispatch } from 'react-redux';
 import Avatar from '@components/avatar';
-import hat from '@src/assets/images/hat.png';
+import hat from '@src/assets/images/hat.svg';
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg';
 import RatingBadge from '../../@core/components/rating-group/RatingBadge';
 import BadgeGroup from '../../@core/components/badge-group-dynamic-count';
