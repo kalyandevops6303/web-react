f6d2084e10801f3200769d0b6549a33a68e3b02e
Nilesh <nilesh.dangi@wowlabz.com>
2023-09-28 15:09:23 +0530

    TD-731 - Rating issue fixed Implemented: Rating upto 2 decimal place showed in invite screen in create project flow

--- src/views/CreateProject/steps/Invite.js
+++ src/views/CreateProject/steps/Invite.js
@@ -27,7 +27,7 @@ import InfiniteScroll from '../../../lib/infinite-scroll';
 import theme from '../../../configs/themeVariables';
 import { BlueBgIconContainer, NavsContainer, TableContainer } from '../style';
 import AlmaMaterImg from '../../../assets/images/almaMater.png';
-import { giveStrokeColor } from '../../../utility/Utils';
+import { giveStrokeColor, returnFormattedRating } from '../../../utility/Utils';
 import NoDataFoundGif from '../../../assets/images/noDataFoundGif.gif';
 import InviteModal from '../InviteModal';
 import SendInvitationModal from '../../modals/SendInvitationModal';
@@ -348,7 +348,7 @@ const Invite = ({ stepper }) => {
                                     fill={theme.starRatingBg}
                                     className="me-50"
                                   />
-                                  <p className="m-0 fw-bolder rating-text">{item.rating}</p>
+                                  <p className="m-0 fw-bolder rating-text">{returnFormattedRating(item.rating)}</p>
                                 </div>
                               </Badge>
                               <p className="m-0 font-small-3 fw-bold ms-1">{item.projects_worked_on_count} Projects</p>
@@ -440,7 +440,9 @@ const Invite = ({ stepper }) => {
                                     fill={theme.starRatingBg}
                                     className="me-50"
                                   />
-                                  <p className="m-0 fw-bolder rating-text">{item.talent_details.rating}</p>
+                                  <p className="m-0 fw-bolder rating-text">
+                                    {returnFormattedRating(item.talent_details.rating)}
+                                  </p>
                                 </div>
                               </Badge>
                               <p className="m-0 font-small-3 fw-bold ms-1">
@@ -530,7 +532,7 @@ const Invite = ({ stepper }) => {
                                     fill={theme.starRatingBg}
                                     className="me-50"
                                   />
-                                  <p className="m-0 fw-bolder rating-text">{item.rating}</p>
+                                  <p className="m-0 fw-bolder rating-text">{returnFormattedRating(item.rating)}</p>
                                 </div>
                               </Badge>
                               <p className="m-0 font-small-3 fw-bold ms-1">{item.projects_worked_on_count} Projects</p>
