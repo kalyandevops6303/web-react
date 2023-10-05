1a82c7505d66e0cec23f2268838726653d9e63dc
Jagannath <142201785+Jagannathkv@users.noreply.github.com>
2023-10-06 00:15:23 +0530

    fixed avatar group issue in upcoming projects card

--- src/views/dashboard/overview/UpcomingProjectCard.js
+++ src/views/dashboard/overview/UpcomingProjectCard.js
@@ -19,7 +19,9 @@ const UpcomingProjectCard = ({ data, className }) => {
     <ProjectWrapper className={className}>
       <Card className="card-app-design">
         <CardBody>
-          <p className="active-project-name">{data?.name}</p>
+          <p className="active-project-name truncate-2" style={{ height: '40px' }}>
+            {data?.name}
+          </p>
           <div className="team-badge px-1">
             <p className="mb-25">Team</p>
           </div>
