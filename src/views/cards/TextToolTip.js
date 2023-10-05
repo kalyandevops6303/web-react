3588fe6c7682416483c7c6fc10f3a5bb2bd80078
Jagannath <142201785+Jagannathkv@users.noreply.github.com>
2023-10-05 23:41:23 +0530

    fixed tooltip issue in client card

--- src/views/cards/TextToolTip.js
+++ src/views/cards/TextToolTip.js
@@ -5,10 +5,10 @@ const TextToolTip = ({ text, id }) => {
   const isLongText = text?.length > 8;
   return (
     <div>
-      <span id={`tooltip-${id}`} className={`${isLongText ? 'truncate-1' : ''}`}>
+      <span id={id} className={isLongText ? 'truncate-1' : ''}>
         {text}
       </span>
-      {isLongText ? <UncontrolledTooltip target={`tooltip-${id}`}>{text}</UncontrolledTooltip> : null}
+      {isLongText && id ? <UncontrolledTooltip target={id}>{text}</UncontrolledTooltip> : null}
     </div>
   );
 };
