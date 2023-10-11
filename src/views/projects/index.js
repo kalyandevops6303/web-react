27c4ed5b24c912c6a504aa4704cbf5cfddeb16db
Nilesh <nilesh.dangi@wowlabz.com>
2023-10-11 13:10:49 +0530

    TD-731 - Create project CTA issues fixed Implemented: Create project CTA in Projects and My Teams page made same as in Dashboard and Marketplace so as to support opening up complete profile modal if profile is not completed and redirection to create-project if profile is completed

--- src/views/projects/index.js
+++ src/views/projects/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import BreadCrumbs from '@components/breadcrumbs';
@@ -7,9 +7,12 @@ import styled from 'styled-components';
 import { useIsTab } from '../../utility/Utils';
 import SecondaryFilters from './overview/SecondaryFilter';
 import PrimaryFilter from './overview/PrimaryFilter';
-import { userData } from '../../redux/selectors/dashboardSelectors';
+import { profilePercentage, userData } from '../../redux/selectors/dashboardSelectors';
 import { clearProjectData } from '../../redux/reducers/projectDetails';
 import { getItem, setItem } from '../../utility/localStorageControl';
+import { userTypes } from '../../utility/constants/Constant';
+import CompleteProfileModal from '../modals/CompleteProfileModal';
+import { getProfilePercentage } from '../../redux/actions/dashboardActions';
 
 const ProjectContainer = styled.div`
   @media only screen and (max-device-width: 600px) {
@@ -22,19 +25,29 @@ const ProjectContainer = styled.div`
 const Projects = () => {
   // Primary filters
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userDetailsData = useSelector(userData);
+  const profilePercentageData = useSelector(profilePercentage);
   const isTab = useIsTab();
 
   // Adjust the number of lines based on the desired limit
 
+  const [completeProfileModal, setCompleteProfileModal] = useState(null);
+  const [completeProfileModalInfoText, setCompleteProfileModalInfoText] = useState(null);
+
   // eslint-disable-next-line no-undef
   const [primaryFilter, setPrimaryFilter] = useState(getItem('selectedProjectTab') ?? 'ONGOING');
+
+  const toggleCompleteProfileModal = () => {
+    setCompleteProfileModal(!completeProfileModal);
+  };
 
   useEffect(() => {
     // eslint-disable-next-line no-undef
     window.scrollTo(0, 0);
     dispatch(clearProjectData());
     setItem('baseRoute', 'projects');
+    dispatch(getProfilePercentage());
   }, []);
 
   // Secondary filters
@@ -53,17 +66,35 @@ const Projects = () => {
     DISPUTE: 'Disputed',
   };
 
+  const onCreateProjectClick = () => {
+    if (
+      profilePercentageData?.values_missing?.includes('company_name') ||
+      profilePercentageData?.values_missing?.includes('educational_institute') ||
+      profilePercentageData?.values_missing?.includes('availability')
+    ) {
+      setCompleteProfileModalInfoText('project');
+      setCompleteProfileModal(true);
+    } else {
+      navigate('/create-project');
+    }
+  };
+
   return (
     <ProjectContainer>
-      <div className="d-flex justify-content-between">
+      {completeProfileModal && (
+        <CompleteProfileModal
+          modal={completeProfileModal}
+          toggleModal={toggleCompleteProfileModal}
+          modalInfoText={completeProfileModalInfoText}
+        />
+      )}
+      <div className="d-flex justify-content-between align-items-center">
         <BreadCrumbs data={[{ title: 'Project', link: '/projects' }, { title: primaryEnum[primaryFilter] }]} />
 
-        {userDetailsData?.user_type === 'CLIENT' && (
-          <Link to="/create-project">
-            <Button as="link" color="primary">
-              Create Project
-            </Button>
-          </Link>
+        {userDetailsData?.user_type === userTypes.client && (
+          <Button as="link" color="primary" onClick={onCreateProjectClick} className="mb-2">
+            Create Project
+          </Button>
         )}
       </div>
       <PrimaryFilter
