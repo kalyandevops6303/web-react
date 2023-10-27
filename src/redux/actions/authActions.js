b259840a54a6cc023bf1e2e294aefd3751b7a4fd
Sourab Patil <sourab.patil@wowlabz.com>
2023-10-27 13:34:56 +0530

    Implemented: Comet Chat Token for Google sign in

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -124,6 +124,7 @@ const loginUserWithGoogle =
       setItem('access_token', res.data.data.access_token);
       if (res.data?.data?.checkpoint === checkPoints.COMPLETE) {
         dispatch(loginSuccess(res.data.data));
+        dispatch(cometChatLogin(res.data.data.comet_chat_token));
       } else {
         dispatch(loginSuccess(false));
       }
