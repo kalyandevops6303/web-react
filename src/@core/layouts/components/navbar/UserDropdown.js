ea9d6381bb62e93dda78b903abb39025a1ce4afd
Aditya <aditya.puri@wowlabz.com>
2023-11-09 16:34:56 +0530

    feature: user dropdown new UI implemented

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -9,7 +9,17 @@ import Avatar from '@components/avatar';
 import { User, Power, Check, CheckCircle } from 'react-feather';
 
 // ** Reactstrap Imports
-import { UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem, UncontrolledTooltip } from 'reactstrap';
+import {
+  UncontrolledDropdown,
+  DropdownMenu,
+  DropdownToggle,
+  DropdownItem,
+  UncontrolledTooltip,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionBody,
+} from 'reactstrap';
 
 // ** Default Avatar Image
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg';
@@ -77,6 +87,10 @@ const UserDropdown = () => {
     await messaging.deleteToken();
     await CometChat.logout();
   };
+
+  const [open, setOpen] = useState('');
+  const toggle = (id) => (open === id ? setOpen() : setOpen(id));
+
   const LineWrapper = styled.div`
     position: relative;
     .line {
@@ -104,19 +118,36 @@ const UserDropdown = () => {
       color: ${theme.red};
       padding: 1rem 1.2rem;
       display: block;
+      border-top: 1px solid ${theme.cardHeaderBorderColor};
+      margin-top: 1rem;
     }
     .edit {
       color: ${theme.primary};
       padding: 1rem 1.2rem;
       display: block;
-      border-top: 1px solid ${theme.cardHeaderBorderColor};
-      margin-top: 1rem;
+      border-bottom: 1px solid ${theme.cardHeaderBorderColor};
       &:active {
         color: white;
       }
     }
+
     .dropdown-item {
       width: 100%;
+    }
+    .edit-accordion {
+      border-bottom: 1px solid ${theme.cardHeaderBorderColor};
+      margin-bottom: 1rem;
+    }
+    .accordion-button {
+      font-size: 14px !important;
+      font-weight: normal !important;
+    }
+    .accordion-body {
+      padding: 0;
+      margin-bottom: 1rem;
+    }
+    .edit-link {
+      padding: 1rem 1.2rem;
     }
   `;
 
@@ -142,6 +173,70 @@ const UserDropdown = () => {
       : savedUserDetails?.client_info?.first_name + ' ' + savedUserDetails?.client_info?.last_name
     : '';
 
+  const handleEditProfileForTeam = () => {
+    navigate('/create-team/profile-details', {
+      state: { isEditing: true },
+    });
+  };
+
+  const handleEditProfileForTalent = (tab) => {
+    if (tab === 'account') {
+      navigate('/talent-onboarding/account-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'personal') {
+      navigate('/talent-onboarding/personal-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'education') {
+      navigate('/talent-onboarding/educational-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'availability') {
+      navigate('/talent-onboarding/availability-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'social') {
+      navigate('/talent-onboarding/social-details', {
+        state: { isEditing: true },
+      });
+    } else {
+      navigate('/talent-onboarding/payment-details', {
+        state: { isEditing: true },
+      });
+    }
+  };
+  const handleEditProfileForClient = (tab) => {
+    if (tab === 'account') {
+      navigate('/client-onboarding/account-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'personal') {
+      navigate('/client-onboarding/personal-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'education') {
+      navigate('/client-onboarding/educational-details', {
+        state: { isEditing: true },
+      });
+    } else if (tab === 'availability') {
+      navigate('/client-onboarding/availability-details', {
+        state: { isEditing: true },
+      });
+    } else {
+      navigate('/client-onboarding/social-details', {
+        state: { isEditing: true },
+      });
+    }
+  };
+
+  const handleEditProfileForClub = (tab) => {
+    if (tab === 'account') {
+      navigate('/create-club/account-details', { state: { isEditing: true } });
+    } else {
+      navigate('/create-club/profile-details', { state: { isEditing: true } });
+    }
+  };
   return (
     <UncontrolledDropdown
       tag="li"
@@ -201,6 +296,107 @@ const UserDropdown = () => {
 
       <UserDropDownWrapper>
         <DropdownMenu end>
+          <DropdownItem onClick={handleEdit} className="w-100 edit">
+            <span className="align-middle ">Public Profile</span>
+          </DropdownItem>
+          <div className="edit-accordion">
+            <Accordion open={open} toggle={toggle}>
+              <AccordionItem>
+                <AccordionHeader className={open === '1' ? 'isActive' : ''} targetId="1">
+                  Edit Profile
+                </AccordionHeader>
+
+                <div style={{ maxHeight: '9rem', overflowY: 'auto' }}>
+                  <AccordionBody accordionId="1">
+                    {userDetailsData?.user_type === userTypes.talent && (
+                      <>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForTalent('account')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Account</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForTalent('personal')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Personal</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForTalent('education')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Education</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForTalent('availability')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Availability</span>
+                        </DropdownItem>
+                        <DropdownItem onClick={() => handleEditProfileForTalent('social')} className="w-100 edit-link ">
+                          <span className="align-middle p-1">Social</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForTalent('payment')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Payment</span>
+                        </DropdownItem>
+                      </>
+                    )}
+                    {userDetailsData?.user_type === userTypes.client && (
+                      <>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForClient('account')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Account</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForClient('personal')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Personal</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForClient('education')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Education</span>
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => handleEditProfileForClient('availability')}
+                          className="w-100 edit-link "
+                        >
+                          <span className="align-middle p-1">Availability</span>
+                        </DropdownItem>
+                        <DropdownItem onClick={() => handleEditProfileForClient('social')} className="w-100 edit-link ">
+                          <span className="align-middle p-1">Social</span>
+                        </DropdownItem>
+                      </>
+                    )}
+                    {userDetailsData?.user_type === userTypes.team && userDetailsData?.team_type === 'CLUB' && (
+                      <>
+                        <DropdownItem onClick={() => handleEditProfileForClub('account')} className="w-100 edit-link">
+                          <span className="align-middle p-1">Account</span>
+                        </DropdownItem>
+                        <DropdownItem onClick={() => handleEditProfileForClub('profile')} className="w-100 edit-link">
+                          <span className="align-middle p-1">Profile</span>
+                        </DropdownItem>
+                      </>
+                    )}
+                    {userDetailsData?.user_type === userTypes.team && userDetailsData?.team_type === 'TEAM' && (
+                      <DropdownItem onClick={handleEditProfileForTeam} className="w-100 edit-link ">
+                        <span className="align-middle p-1">Profile</span>
+                      </DropdownItem>
+                    )}
+                  </AccordionBody>
+                </div>
+              </AccordionItem>
+            </Accordion>
+          </div>
+
           <div style={{ maxHeight: '13rem', overflowY: 'auto' }}>
             {userDetailsData && (
               <DropdownItem
@@ -278,9 +474,7 @@ const UserDropdown = () => {
               </DropdownItem>
             ))}
           </div>
-          <DropdownItem onClick={handleEdit} className="w-100 edit">
-            <span className="align-middle ">Profile</span>
-          </DropdownItem>
+
           <DropdownItem onClick={handleLogout} className="w-100 logout">
             <span className="align-middle ">Logout</span>
           </DropdownItem>
