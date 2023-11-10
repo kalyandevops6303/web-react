4706a3a9ab754425aed283066f15baa5ae2bd6cf
Sourab Patil <sourab.patil@wowlabz.com>
2023-11-10 15:13:44 +0530

    Fix: Cometchat api inside useEffect added

--- src/@core/layouts/components/navbar/index.js
+++ src/@core/layouts/components/navbar/index.js
@@ -86,12 +86,14 @@ const ThemeNavbar = (props) => {
     }
   }, []);
 
-  if (isCometChatLoggedIn) {
-    CometChat.getUnreadMessageCountForAllUsers().then((unreadMsgs) => {
-      const totalCount = Object.values(unreadMsgs).reduce((acc, count) => acc + count, 0);
-      dispatch(setUnreadMsgCount(totalCount));
-    });
-  }
+  useEffect(() => {
+    if (isCometChatLoggedIn) {
+      CometChat.getUnreadMessageCountForAllUsers().then((unreadMsgs) => {
+        const totalCount = Object.values(unreadMsgs).reduce((acc, count) => acc + count, 0);
+        dispatch(setUnreadMsgCount(totalCount));
+      });
+    }
+  }, [isCometChatLoggedIn]);
 
   useEffect(() => {
     if (location?.pathname?.split('/')?.[1] === 'dashboard') dispatch(setActiveNavTab('dashboard'));
