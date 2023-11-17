c6bde1d73da084d3d4e89fe01d7dbcb81b514893
Nilesh <nilesh.dangi@wowlabz.com>
2023-11-17 13:03:14 +0530

    fix - tooltip id crashing the dashboard issue fixed in all the active project cards, upcoming project cards and payments for all the user types

--- src/views/dashboard/overview/UpcomingPaymentsCard.js
+++ src/views/dashboard/overview/UpcomingPaymentsCard.js
@@ -61,10 +61,10 @@ const UpcomingPaymentsCard = ({ data, className }) => {
                       placement: 'bottom',
                       imgHeight: 33,
                       imgWidth: 33,
-                      tooltipId: `tooltip_${data?._id}_${worker?.first_name.replace(
+                      tooltipId: `tooltip-${worker?.first_name?.replace(/\s+/g, '-')}-${worker?.last_name?.replace(
                         /\s+/g,
                         '-',
-                      )}_${data?.current_milestone?.name.replace(/\s+/g, '-')}`,
+                      )}`,
                     })),
                   ]}
                 />
@@ -79,10 +79,10 @@ const UpcomingPaymentsCard = ({ data, className }) => {
                     placement: 'bottom',
                     imgHeight: 33,
                     imgWidth: 33,
-                    tooltipId: `tooltip_${data?._id}_${worker?.first_name.replace(
+                    tooltipId: `tooltip-${worker?.first_name?.replace(/\s+/g, '-')}-${worker?.last_name?.replace(
                       /\s+/g,
                       '-',
-                    )}_${data?.current_milestone?.name.replace(/\s+/g, '-')}`,
+                    )}`,
                   })),
                 ]}
               />
