d851c594e83ec84d1331b359db9182c726748073
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2023-11-24 16:29:54 +0530

    Fix: Text changes in milestones

--- src/views/modals/MakePaymentModal.js
+++ src/views/modals/MakePaymentModal.js
@@ -164,9 +164,7 @@ function MakePaymentModal({ toggleModal, modal, selectedMilestoneIds }) {
                     >
                       {getTagSettings(item.payment_status).text}
                     </Badge>
-                    <div
-                      style={{ fontSize: '16px', fontWeight: '500' }}
-                    >{`$ ${item.estimated_cost.toLocaleString()}`}</div>
+                    <div style={{ fontSize: '16px', fontWeight: '500' }}>{`$ ${item.estimated_cost}`}</div>
                   </CardBody>
                 </Card>
               ))}
@@ -178,9 +176,7 @@ function MakePaymentModal({ toggleModal, modal, selectedMilestoneIds }) {
                   <CardText style={{ fontSize: '16px' }}>{`${applicationFee?.name ?? ''} (${
                     applicationFee?.percentage ?? 0
                   }%)`}</CardText>
-                  <CardText style={{ fontSize: '16px' }}>{`$ ${
-                    Number.isNaN(trumioFee) ? 0 : trumioFee.toLocaleString()
-                  }`}</CardText>
+                  <CardText style={{ fontSize: '16px' }}>{`$ ${Number.isNaN(trumioFee) ? 0 : trumioFee}`}</CardText>
                 </div>
                 <hr className="m-0 card-header-border" />
                 <div className="d-flex justify-content-between p-1">
