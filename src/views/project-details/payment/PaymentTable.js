0f1b45ef1ee3a84f7f5de94e5e148760d106d09e
Vighnesh <133745608+vighneshc21@users.noreply.github.com>
2024-02-29 16:05:58 +0530

    Fix: Milestone payment selection reverted from payment table

--- src/views/project-details/payment/PaymentTable.js
+++ src/views/project-details/payment/PaymentTable.js
@@ -108,47 +108,16 @@ const PaymentTable = () => {
     }
   }, [projectDetailsData?._id]);
 
-  useEffect(() => {
-    if (milestoneData?.length > 1) {
-      setSelectedPaymentData([milestoneData[0], milestoneData[1]]);
-      setSelectedPaymentId([milestoneData[0]._id, milestoneData[1]._id]);
-    }
-    if (milestoneData?.length === 1) {
-      setSelectedPaymentData([milestoneData[0]]);
-      setSelectedPaymentId([milestoneData[0]._id]);
-    }
-  }, [milestoneData]);
-
   const handlePaymentSelect = (e) => {
     const id = e.target.name;
     const isExisting = selectedPaymentData.find((item) => item._id === id);
-
-    // Check if the clicked item is within the first two values of milestoneData
-    const isFirstTwo = milestoneData.slice(0, 2).some((item) => item._id === id);
-
-    // If the clicked item is not in the first two values of milestoneData and
-    // there are already 2 items selected, allow selection/deselection
-    if (!isFirstTwo && selectedPaymentData.length >= 2) {
-      // If the clicked item is already selected, remove it
-      if (isExisting) {
-        const newArray = selectedPaymentData.filter((item) => item._id !== id);
-        const newData = selectedPaymentId.filter((item) => item !== id);
-        setSelectedPaymentData(newArray);
-        setSelectedPaymentId(newData);
-      } else {
-        // If not selected, add it
-        const selectedTransaction = milestoneData.find((item) => item._id === id);
-        setSelectedPaymentData((prev) => [...prev, selectedTransaction]);
-        setSelectedPaymentId((prev) => [...prev, id]);
-      }
-    } else if (isFirstTwo) {
-      // If the clicked item is in the first two values of milestoneData
-      // If the clicked item is already selected, return to prevent deselection
-      if (isExisting) {
-        return;
-      }
-
-      // Add the clicked item to selectedPaymentData
+    if (isExisting) {
+      const newArray = selectedPaymentData.filter((item) => item._id !== id);
+      const newData = selectedPaymentId.filter((item) => item !== id);
+      setSelectedPaymentData(newArray);
+      setSelectedPaymentId(newData);
+    }
+    if (!isExisting) {
       const selectedTransaction = milestoneData.find((item) => item._id === id);
       setSelectedPaymentData((prev) => [...prev, selectedTransaction]);
       setSelectedPaymentId((prev) => [...prev, id]);
