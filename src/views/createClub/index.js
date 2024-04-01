64ecaab91876af7cd121a8a3dd0da54ba5fb9944
Nilesh <nilesh.dangi@wowlabz.com>
2024-04-01 17:50:41 +0530

    Fix - added top navbar in the create club and create team and also added top back button like in create project (PT-109,110)

--- src/views/createClub/index.js
+++ src/views/createClub/index.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { ArrowLeft } from 'react-feather';
-import Header from '../Onboarding/Header';
 import Tabs from './Tabs';
 import { BackButtonContainer } from '../Onboarding/style';
 import theme from '../../configs/themeVariables';
 import { getItemFromSession, removeItemFromSession } from '../../utility/sessesionStorageControl';
 import { setActiveNavTab } from '../../redux/reducers/activeNavTab';
+import { CircularBackButtonContainer } from '../styled';
 
 const CreateClub = () => {
   const tabNames = {
@@ -35,27 +35,31 @@ const CreateClub = () => {
   }, []);
 
   return (
-    <>
-      {location.pathname.includes('create-club') && <Header />}
-
-      <div className={`${location.pathname.includes('create-club') ? 'px-5 py-3' : 'px-3 pt-1'} `}>
-        <div className="px-2">
-          {location.pathname.includes('create-club') ? (
-            <h2>Create Club</h2>
-          ) : (
-            <BackButtonContainer>
-              <div className="d-flex align-items-center upload-button cursor-pointer" onClick={onBackClick}>
-                <div className="add-icon-container">
-                  <ArrowLeft size={18} color={theme.white} />
-                </div>
-                <h5 className="fw-normal">Back</h5>
+    <div className={`${location.pathname.includes('create-club') ? 'px-5 py-2' : 'px-3 pt-1'} `}>
+      <div className="px-2">
+        {location.pathname.includes('create-club') ? (
+          <CircularBackButtonContainer
+            className="d-flex align-items-center cursor-pointer"
+            onClick={() => navigate('/dashboard')}
+          >
+            <div className="back-icon-container">
+              <ArrowLeft size={18} color={theme.white} />
+            </div>
+            <p className="fw-light mb-0 ms-50 font-medium-2">Create Club</p>
+          </CircularBackButtonContainer>
+        ) : (
+          <BackButtonContainer>
+            <div className="d-flex align-items-center upload-button cursor-pointer" onClick={onBackClick}>
+              <div className="add-icon-container">
+                <ArrowLeft size={18} color={theme.white} />
               </div>
-            </BackButtonContainer>
-          )}
-          <Tabs tabNames={tabNames} active={active} />
-        </div>
+              <h5 className="fw-normal">Back</h5>
+            </div>
+          </BackButtonContainer>
+        )}
+        <Tabs tabNames={tabNames} active={active} />
       </div>
-    </>
+    </div>
   );
 };
 
--- src/views/createTeam/index.js
+++ src/views/createTeam/index.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { ArrowLeft } from 'react-feather';
-import Header from '../Onboarding/Header';
 import Tabs from './Tabs';
 import { BackButtonContainer } from '../Onboarding/style';
 import theme from '../../configs/themeVariables';
 import { getItemFromSession, removeItemFromSession } from '../../utility/sessesionStorageControl';
 import { setActiveNavTab } from '../../redux/reducers/activeNavTab';
 import { userProfileEdit } from '../../utility/constants/Constant';
+import { CircularBackButtonContainer } from '../styled';
 
 const CreateTeam = () => {
   const tabNames = {
@@ -39,27 +39,31 @@ const CreateTeam = () => {
   }, []);
 
   return (
-    <>
-      {location.pathname.includes('create-team') && <Header />}
-
-      <div className={`${location.pathname.includes('create-team') ? 'px-5 py-3' : 'px-3 pt-1'} `}>
-        <div className="px-2">
-          {location.pathname.includes('create-team') ? (
-            <h2>Create Team</h2>
-          ) : (
-            <BackButtonContainer>
-              <div className="d-flex align-items-center upload-button cursor-pointer" onClick={onBackClick}>
-                <div className="add-icon-container">
-                  <ArrowLeft size={18} color={theme.white} />
-                </div>
-                <h5 className="fw-normal">Back</h5>
+    <div className={`${location.pathname.includes('create-team') ? 'px-5 py-2' : 'px-3 pt-1'} `}>
+      <div className="px-2">
+        {location.pathname.includes('create-team') ? (
+          <CircularBackButtonContainer
+            className="d-flex align-items-center cursor-pointer"
+            onClick={() => navigate('/dashboard')}
+          >
+            <div className="back-icon-container">
+              <ArrowLeft size={18} color={theme.white} />
+            </div>
+            <p className="fw-light mb-0 ms-50 font-medium-2">Create Team</p>
+          </CircularBackButtonContainer>
+        ) : (
+          <BackButtonContainer>
+            <div className="d-flex align-items-center upload-button cursor-pointer" onClick={onBackClick}>
+              <div className="add-icon-container">
+                <ArrowLeft size={18} color={theme.white} />
               </div>
-            </BackButtonContainer>
-          )}
-          <Tabs tabNames={tabNames} active={active} />
-        </div>
+              <h5 className="fw-normal">Back</h5>
+            </div>
+          </BackButtonContainer>
+        )}
+        <Tabs tabNames={tabNames} active={active} />
       </div>
-    </>
+    </div>
   );
 };
 
