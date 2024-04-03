a700e8bc90789b2294fbfcbff89dc523a19b23df
Nilesh <nilesh.dangi@wowlabz.com>
2024-04-03 12:05:42 +0530

    Fix - added project name redirection with switch profile capability, client name redirection and team name redirection in payments history table for all user types

--- src/views/paymentFullView/overview/PaymentHistory.js
+++ src/views/paymentFullView/overview/PaymentHistory.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unsafe-optional-chaining */
 /* eslint-disable consistent-return */
 /* eslint-disable no-else-return */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Proptypes from 'prop-types';
@@ -21,6 +21,7 @@ import { paymentHistory, paymentHistoryLoading } from '../../../redux/selectors/
 import ComponentSpinner from '../../../@core/components/spinner/Loading-spinner';
 import capitalize from '../../../lib/capitalize';
 import { setItem } from '../../../utility/localStorageControl';
+import SwitchConfirmModal from '../../modals/SwitchConfirm';
 
 const PaymentHistory = () => {
   const dispatch = useDispatch();
@@ -29,6 +30,9 @@ const PaymentHistory = () => {
   const userData = useSelector(selectUserData);
   const paymentHistoryData = useSelector(paymentHistory);
   const paymentHistoryIsLoading = useSelector(paymentHistoryLoading);
+
+  const [switchProfileModal, setSwitchProfileModal] = useState(false);
+  const [switchData, setSwitchData] = useState();
 
   const showStatusBadge = (status) => {
     if (status === 'PAID') {
@@ -237,7 +241,23 @@ const PaymentHistory = () => {
         ),
         project_name: (
           <>
-            <p className="mb-0 font-small-4 name-ellipsis" id={`project-${item?.transaction_id}`}>
+            <p
+              className="mb-0 font-small-4 name-ellipsis cursor-pointer"
+              id={`project-${item?.transaction_id}`}
+              onClick={() => {
+                setItem('baseRoute', 'payments');
+                if (userData?.user_type === userTypes.talent && '_id' in item?.team) {
+                  setSwitchData({
+                    entity: 'TEAM',
+                    navigateTo: `/project-details/${item?.project_id}/payment`,
+                    switchTeamId: item?.team?._id,
+                  });
+                  setSwitchProfileModal(true);
+                } else {
+                  navigate(`/project-details/${item?.project_id}/payment`);
+                }
+              }}
+            >
               {item?.project_name}
             </p>
             <UncontrolledTooltip target={`project-${item?.transaction_id}`}>{item?.project_name}</UncontrolledTooltip>
@@ -286,7 +306,13 @@ const PaymentHistory = () => {
           </div>
         ),
         client_name: (
-          <div className="d-flex align-items-center">
+          <div
+            className="d-flex align-items-center cursor-pointer"
+            onClick={() => {
+              setItem('baseRoute', 'payments');
+              navigate(`/profile/client/${item?.client?.user_id}`);
+            }}
+          >
             <Avatar
               img={item?.client?.image_uri || defaultAvatar}
               imgHeight="28"
@@ -298,7 +324,17 @@ const PaymentHistory = () => {
             </p>
           </div>
         ),
-        team_name: <>{showTeamDetails(item?.team?.name, item?.team?.logo)}</>,
+        team_name: (
+          <p
+            className="mb-0 cursor-pointer"
+            onClick={() => {
+              setItem('baseRoute', 'payments');
+              navigate(`/profile/team/${item?.team?._id}`);
+            }}
+          >
+            {showTeamDetails(item?.team?.name, item?.team?.logo)}
+          </p>
+        ),
         status: <>{showStatusBadge(item?.status)}</>,
         pay_type: <p className="mb-0 font-small-4">{capitalize(item?.pay_type)}</p>,
         total_cost: <p className="mb-0 font-small-4">${item?.total_cost}</p>,
@@ -378,6 +414,15 @@ const PaymentHistory = () => {
 
   return (
     <>
+      {switchProfileModal && (
+        <SwitchConfirmModal
+          entity={switchData?.entity}
+          navigateTo={switchData?.navigateTo}
+          switchTeamId={switchData?.switchTeamId}
+          modal={switchProfileModal}
+          toggleModal={() => setSwitchProfileModal(!switchProfileModal)}
+        />
+      )}
       <p className="fw-bold font-medium-3 mt-1">Payment History</p>
       {paymentHistoryIsLoading ? (
         <ComponentSpinner />
