ce4c922ebec630d829aeff566ece683f6e55ce31
Palahana <ahana.pal@wowlabz.com>
2024-05-02 16:36:15 +0530

    feat: Storing user_id in localstorage for GA during google sign in

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -138,6 +138,7 @@ const loginUserWithGoogle =
       setItem('access_token_expires', res.data.data.access_token_expires);
       setItem('refresh_token', res.data.data.refresh_token);
       setItem('refresh_token_expires', res.data.data.refresh_token_expires);
+      setItem('user_id',res.data.data.user_id);
       if (res.data?.data?.checkpoint === checkPoints.COMPLETE) {
         dispatch(loginSuccess(res.data.data));
         dispatch(cometChatLogin(res.data.data.comet_chat_token));
