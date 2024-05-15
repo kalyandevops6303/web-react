a8dbe8424402a913040fe109223f943e616e2415
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2024-05-15 15:33:07 +0530

    Fix: Removed logic of checking status from payment view and added to project details index
    - Removed logic of checking status from payment view and added to project details index

--- src/views/project-details/payment/PaymentTab.js
+++ src/views/project-details/payment/PaymentTab.js
@@ -1,33 +1,15 @@
 /* eslint-disable no-undef */
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import PaymentTable from './PaymentTable';
 import PaymentHistoryTable from './PaymentHistoryTable';
-import { updatePaymentStatus } from '../../../redux/actions/milestonePaymentActions';
 import ComponentSpinner from '../../../@core/components/spinner/Loading-spinner';
-import { CHECKOUT_STATUS } from '../../../utility/constants/Constant';
 
 function PaymentTab() {
-  // Get the query parameters from the URL
-  const queryParams = new URLSearchParams(window.location.search);
-
-  // Extract the checkout status
-  const checkoutStatus = queryParams.get('checkout_status');
-  const sessionId = queryParams.get('session_id');
-
-  const dispatch = useDispatch();
   const isStatusUpdating = useSelector((state) => state.milestonePayment.paymentStatusUpdating);
-
-  useEffect(() => {
-    if (checkoutStatus === CHECKOUT_STATUS.CANCELLED) {
-      dispatch(updatePaymentStatus({ session_id: sessionId }));
-    }
-  }, [checkoutStatus]);
-
   if (isStatusUpdating) {
     return <ComponentSpinner />;
   }
-
   return (
     <>
       <PaymentTable />
