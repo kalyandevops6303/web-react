ac14c6f5d71cfc397d978aa326313ba7c08531f2
Vighnesh <vighnesh.chaurasia@wowlabz.com>
2024-05-27 15:28:17 +0530

    Fix: Usertype sometime was getting passed even if state is cleared
    - script.onload capture the values of variables at the time they were created. This means that if userType changes after the onload callback is set up, the callback would still hold the old value. Using useRef allows us to maintain a reference to the latest userType

--- src/views/auth/components/SigninWithGoogle.js
+++ src/views/auth/components/SigninWithGoogle.js
@@ -2,7 +2,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { memo, useEffect } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import { loginUserWithGoogle } from '../../../redux/actions/authActions';
 import { selectUserType } from '../../../redux/selectors/authSelectors';
 import { ERROR_CODES, checkPoints } from '../../../utility/constants/Constant';
@@ -11,6 +11,21 @@ const SigninWithGoogle = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userType = useSelector(selectUserType);
+  const userTypeRef = useRef(userType);
+
+  {
+    /*  script.onload capture the values of variables at the time they were created. 
+    This means that if userType changes after the onload callback is set up, 
+    the callback would still hold the old value. Using useRef allows us to maintain 
+    a reference to the latest userType
+    */
+  }
+  // Update the ref whenever userType changes
+  useEffect(() => {
+    userTypeRef.current = userType;
+  }, [userType]);
+
+  // Styled-component
   const GoogleWrap = styled.div`
     display: flex;
     justify-content: center;
@@ -24,6 +39,7 @@ const SigninWithGoogle = () => {
     }
   `;
 
+  // Success handler for Google login
   const onSuccess = (resp) => {
     if (resp?.checkpoint === checkPoints.MOBILE_VERIFICATION) {
       navigate('/auth/register-phone');
@@ -35,15 +51,18 @@ const SigninWithGoogle = () => {
       navigate('/dashboard');
     }
   };
+
+  // Error handler for Google login
   const onError = (error) => {
     if (error.response.status === ERROR_CODES.EC_422) {
       navigate('/auth');
     }
   };
 
+  // Handle Google credential response
   const handleCredentialResponse = (response) => {
-    // Handle the credential response here
-    dispatch(loginUserWithGoogle({ id_token: response.credential, user_type: userType, onError, onSuccess }));
+    const currentUserType = userTypeRef.current;
+    dispatch(loginUserWithGoogle({ id_token: response.credential, user_type: currentUserType, onError, onSuccess }));
   };
 
   useEffect(() => {
@@ -68,6 +87,10 @@ const SigninWithGoogle = () => {
         logo_alignment: 'left',
       });
     };
+
+    return () => {
+      document.body.removeChild(script); // Clean up script element on component unmount
+    };
   }, []);
 
   return (
@@ -76,4 +99,5 @@ const SigninWithGoogle = () => {
     </GoogleWrap>
   );
 };
+
 export default memo(SigninWithGoogle);
