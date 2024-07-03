8875af5920c32ab8e39515622a0ff73388918712
Soumyaraj Bag <soumyaraj@trumio.ai>
2024-07-03 10:56:20 +0530

    fix:comment

--- src/views/Onboarding/Talent/Social.js
+++ src/views/Onboarding/Talent/Social.js
@@ -35,7 +35,7 @@ import {
 import AccountCreatedModal from '../AccountCreatedModal';
 import ShowToastMessage from '../../../@core/components/toast';
 import { ERROR } from '../../../utility/constants/ToastTypes';
-import { filteredFormSchema, formatUrl, isUrlWithoutProtocol, removeEmptyKeys } from '../../../utility/Utils';
+import { filteredFormSchema, formatUrl, isEmpty, isUrlWithoutProtocol, removeEmptyKeys } from '../../../utility/Utils';
 import { userOnboarding, userProfileEdit } from '../../../utility/constants/Constant';
 import ComponentSpinner from '../../../@core/components/spinner/Loading-spinner';
 import { formData, formDocuments, resumeParsed } from '../../../redux/selectors/formDataSelectors';
@@ -569,7 +569,7 @@ const Social = () => {
               </div>
             </Col>
 
-            {files?.length > 0 && (
+            {!isEmpty(files) && (
               <Col>
                 <Card>
                   <CardBody>
