b1c3d04a84925e9c5a25ae36dc1ad9ff04257741
vikas2705 <vikas.vikasranjan@gmail.com>
2024-07-29 17:16:15 +0530

    fix merge changes

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -36,8 +36,6 @@ import FeedbackForCustomerSupportModal from '../../../../views/modals/CustomerSu
 import { setFormDocuments } from '../../../../redux/reducers/formData';
 import UserNameRoleCompanyComp from '../../../components/username-role-company';
 import { setFormDocuments } from '../../../../redux/reducers/formData';
-import DelegateProfileCard from '../../../../views/Onboarding/Delegate/DelegateProfileCard';
-import DelegateCard from '../../../../views/cards/DelegateCard';
 import UserNameRoleCompanyComp from '../../../components/username-role-company';
 
 const UserDropdown = ({ setNavBarLoading }) => {
