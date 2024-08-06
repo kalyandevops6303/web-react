c6ebf9e681f5e0fe92157388fb04829d2510b74d
tr-vikas <vikas@trumio.ai>
2024-08-06 20:03:36 +0530

    Fix create Project includes bug

--- src/views/CreateProject/steps/Requirements.js
+++ src/views/CreateProject/steps/Requirements.js
@@ -146,17 +146,17 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
       .required('Desired time overlap is required'),
     availabilityDays: yup.array().min(1, 'Select at least one work day').required('Select at least one work day'),
     weekdays: yup.array().when('availabilityDays', {
-      is: (availabilityDays) => availabilityDays && availabilityDays.includes('weekdays'),
+      is: (availabilityDays) => availabilityDays && availabilityDays?.includes('weekdays'),
       then: () =>
         yup.array().min(1, 'Select at least one day in the week').required('Select at least one day in the week'),
     }),
     weekends: yup.array().when('availabilityDays', {
-      is: (availabilityDays) => availabilityDays && availabilityDays.includes('weekends'),
+      is: (availabilityDays) => availabilityDays && availabilityDays?.includes('weekends'),
       then: () =>
         yup.array().min(1, 'Select at least one day in the weekend').required('Select at least one day in the weekend'),
     }),
     weekdayStartTime: yup.object().when('availabilityDays', {
-      is: (availabilityDays) => availabilityDays && availabilityDays.includes('weekdays'),
+      is: (availabilityDays) => availabilityDays && availabilityDays?.includes('weekdays'),
       then: () =>
         yup
           .object()
@@ -1090,7 +1090,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                             type="checkbox"
                             {...field}
                             id="weekdays"
-                            checked={field.value.includes('weekdays')}
+                            checked={field.value?.includes('weekdays')}
                             onChange={(e) => {
                               const isChecked = e.target.checked;
                               const value = 'weekdays';
@@ -1111,7 +1111,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                             type="checkbox"
                             {...field}
                             id="weekends"
-                            checked={field.value.includes('weekends')}
+                            checked={field.value?.includes('weekends')}
                             onChange={(e) => {
                               const isChecked = e.target.checked;
                               const value = 'weekends';
@@ -1134,9 +1134,9 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                 </Row>
                 <Row>
                   {availabilityDays &&
-                    (availabilityDays.includes('weekdays') || availabilityDays.includes('weekends')) && (
+                    (availabilityDays?.includes('weekdays') || availabilityDays?.includes('weekends')) && (
                       <>
-                        {availabilityDays.includes('weekdays') && (
+                        {availabilityDays?.includes('weekdays') && (
                           <div>
                             <Row className="mb-1 mt-2">
                               <div className="d-flex align-items-center">
@@ -1238,7 +1238,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="MONDAY"
-                                        checked={field.value.includes('MONDAY')}
+                                        checked={field.value?.includes('MONDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'MONDAY';
@@ -1259,7 +1259,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="TUESDAY"
-                                        checked={field.value.includes('TUESDAY')}
+                                        checked={field.value?.includes('TUESDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'TUESDAY';
@@ -1280,7 +1280,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="WEDNESDAY"
-                                        checked={field.value.includes('WEDNESDAY')}
+                                        checked={field.value?.includes('WEDNESDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'WEDNESDAY';
@@ -1301,7 +1301,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="THURSDAY"
-                                        checked={field.value.includes('THURSDAY')}
+                                        checked={field.value?.includes('THURSDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'THURSDAY';
@@ -1322,7 +1322,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="FRIDAY"
-                                        checked={field.value.includes('FRIDAY')}
+                                        checked={field.value?.includes('FRIDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'FRIDAY';
@@ -1346,7 +1346,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                           </div>
                         )}
 
-                        {availabilityDays.includes('weekends') && (
+                        {availabilityDays?.includes('weekends') && (
                           <div>
                             <Row className="mb-1 mt-2">
                               <div className="d-flex align-items-center">
@@ -1448,7 +1448,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="SATURDAY"
-                                        checked={field.value.includes('SATURDAY')}
+                                        checked={field.value?.includes('SATURDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'SATURDAY';
@@ -1469,7 +1469,7 @@ const Requirements = ({ stepper, setProjectDetails, files, setFiles, setDraftSav
                                         type="checkbox"
                                         {...field}
                                         id="SUNDAY"
-                                        checked={field.value.includes('SUNDAY')}
+                                        checked={field.value?.includes('SUNDAY')}
                                         onChange={(e) => {
                                           const isChecked = e.target.checked;
                                           const value = 'SUNDAY';
