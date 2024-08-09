310af7619e67e27ee9d7f0ce03e71d2fb474c9d0
tr-vikas <vikas@trumio.ai>
2024-08-09 12:10:31 +0530

    Assign Bid Bug

--- src/views/project-details/BidDetails.js
+++ src/views/project-details/BidDetails.js
@@ -67,6 +67,7 @@ const BidDetails = () => {
     navigate(-1);
   };
   const bidInfo = useSelector((state) => state.projectDetails.bidInfo);
+  
   const isLoading = useSelector((state) => state.projectDetails.getBidInfoLoading);
   const downloadUrlIsLoading = useSelector(downloadUrlLoading);
   const bidView = location?.pathname?.split('/')?.slice(0, -1)?.join('/');
@@ -123,13 +124,17 @@ const BidDetails = () => {
   const onDownloadResumeUrlSuccess = ({ download_url, file_name }) => {
     downloadFile({ data: { download_url }, file_name });
   };
-
   const BidDetailsHeaderSection = styled.div`
     .fixed-head {
+      // z-index: 20;
+      // margin-top: -rem;
+      // padding-top: 1rem;
+      // position: fixed;
       width: 100%;
       background: ${theme.bodyBgColor};
       // padding-bottom: 0.8rem;
       padding-left: 0;
+      
       .inner-head {
         display: flex;
         justify-content: space-between;
@@ -145,7 +150,6 @@ const BidDetails = () => {
   `;
 
   if (isLoading) return <ComponentSpinner />;
-
   return (
     <BidDetailsWrap>
       <div className="d-flex justify-content-between mb-2 pb-2 rounded top-head">
