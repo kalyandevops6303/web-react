5680af505a9bc825ba14d507d2da8def09c17806
vikas2705 <vikas.vikasranjan@gmail.com>
2024-08-29 12:01:33 +0530

    fixed bug of tooltip

--- src/views/cards/DelegateNameCard.js
+++ src/views/cards/DelegateNameCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DropdownItem, UncontrolledTooltip } from 'reactstrap';
+import { DropdownItem } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { capitalize } from 'lodash';
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg';
@@ -17,13 +17,6 @@ const DelegateNameCard = ({ img, companyName, userName, icon, userType }) => (
         <span className="user-name fw-bold text-start d-block" id="username">
           {userName}
         </span>
-        {userName?.length > 15 && (
-          <UncontrolledTooltip placement="right" target="username">
-            <div className="d-flex flex-column align-items-start">
-              <p className="text-start m-0">{userName}</p>
-            </div>
-          </UncontrolledTooltip>
-        )}
         {companyName && (
           <span className="w-100 font-small-3 d-block user-status text-start">{capitalize(companyName)}</span>
         )}
