f79cff68d86c8c5ace0eb411caa7d3eb4cad0b41
Notorious97 <sakshamanand150@gmail.com>
2024-09-11 10:34:38 +0530

    fix: change upload apis to use multipart/form-data

--- src/views/create-bid/overview/FixedSimpleMilestoneView.js
+++ src/views/create-bid/overview/FixedSimpleMilestoneView.js
@@ -407,7 +407,7 @@ const FixedSimpleMilestoneView = ({ setDraftSavedModal }) => {
 
       await milestoneFileUploadToAzureService(file.uploadData.upload_url, file.file, {
         'x-ms-blob-type': 'BlockBlob',
-        'Content-Type': file.file.type,
+        'Content-Type': 'multipart/form-data',
       });
     } catch (error) {
       ShowToastMessage(ERROR, 'Something went wrong. Please try uploading again.');
