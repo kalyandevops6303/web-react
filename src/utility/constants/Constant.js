ee669b96621480c034dcd8e71e7a76ead74ff2f6
Soumyaraj Bag <soumyaraj@trumio.ai>
2024-10-03 22:18:59 +0530

    fix:additional-tab

--- src/utility/constants/Constant.js
+++ src/utility/constants/Constant.js
@@ -259,11 +259,11 @@ const PROJECT_INVITATION_STATUS = {
 };
 
 const studyYears = [
-  { label: '2020', value: '2020' },
-  { label: '2021', value: '2021' },
-  { label: '2022', value: '2022' },
-  { label: '2023', value: '2023' },
-  { label: '2024', value: '2024' },
+  { label: '2020', value: 2020 },
+  { label: '2021', value: 2021 },
+  { label: '2022', value: 2022 },
+  { label: '2023', value: 2023 },
+  { label: '2024', value: 2024 },
 ];
 
 export {
