d2e311dc184deee97dd21844dedd50f7311f09fc
saharoysoham <soham.saharoy@trumio.ai>
2024-10-06 23:56:20 +0530

    fix: graduation years values modified

--- src/utility/constants/Constant.js
+++ src/utility/constants/Constant.js
@@ -258,21 +258,26 @@ const PROJECT_INVITATION_STATUS = {
   READ_ONLY: 'READ_ONLY',
 };
 
-const generateStudyYears = () => {
+const generateYearArrays = () => {
   const currentYear = new Date().getFullYear();
   const startYear = currentYear - 7;
-  
-  const years = Array.from(
-    { length: currentYear - startYear + 1 },
-    (_, index) => {
-      const year = startYear + index;
-      return { label: year.toString(), value: year };
-    }
-  );
-  
-  return years;
+  const endYear = currentYear + 4;
+
+  const generateYearRange = (start, end) => 
+    Array.from(
+      { length: end - start + 1 },
+      (_, index) => {
+        const year = start + index;
+        return { label: year.toString(), value: year };
+      }
+    );
+
+  return {
+    studyYears: generateYearRange(startYear, currentYear),
+    graduationYears: generateYearRange(currentYear, endYear)
+  };
 };
-const studyYears = generateStudyYears();
+const {studyYears,graduationYears} = generateYearArrays();
 
 export {
   ERROR_CODES,
@@ -319,4 +324,5 @@ export {
   PaymentStatusOptions,
   PayTypeOptions,
   studyYears,
+  graduationYears,
 };
