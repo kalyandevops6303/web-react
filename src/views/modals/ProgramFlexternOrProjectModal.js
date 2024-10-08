76c382317a80cc7aef4ee622726058c9048bc814
saharoysoham <soham.saharoy@trumio.ai>
2024-10-08 19:06:39 +0530

    feat: fixes for skip in educational details

--- src/views/modals/ProgramFlexternOrProjectModal.js
+++ src/views/modals/ProgramFlexternOrProjectModal.js
@@ -9,7 +9,7 @@ import { Modal, ModalHeader, ModalBody, Input, Row, Col, Button} from 'reactstra
 import toast from 'react-hot-toast';
 import { CreateBidRadioOption } from '../styled';
 import { selectTrumioIsFlextern } from '../../redux/selectors/authSelectors';
-import { getUserDetails, saveTalentAccountDetails } from '../../redux/actions/talentOnboardingActions';
+import { saveTalentAccountDetails } from '../../redux/actions/talentOnboardingActions';
 import { setTalentBooleanTrumioTalent, setTalentBooleansFlextern } from '../../redux/reducers/auth';
 
 
