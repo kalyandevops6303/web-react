1b0ac952e64360c9210e0da29337aab8a401e02a
praneeth-rdy <praneeth@trumio.ai>
2024-11-07 15:40:12 +0530

    feat: added css files as needed for feedback survey

--- src/flexternships/app/components/pages/create-project/tabs/milestone/feedback/HandleFeedbacks.tsx
+++ src/flexternships/app/components/pages/create-project/tabs/milestone/feedback/HandleFeedbacks.tsx
@@ -1,7 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import SelfFeedback from './SelfFeedback';
 
 export default function HandleFeedbacks() {
   const { feedbackType } = useParams();
+  const allowedFeedbackTypes = ['self', 'peer', 'team', 'individual'];
+  if (!feedbackType || !allowedFeedbackTypes.includes(feedbackType)) {
+    return <Navigate to={'/404'} />;
+  }
   return <SelfFeedback />;
 }
