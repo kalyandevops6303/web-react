b87cad6780999997613e9ba36444c8aff4d1f7de
praneeth-rdy <praneeth@trumio.ai>
2024-11-14 21:17:53 +0530

    prettier formatted

--- src/flexternships/app/components/pages/project-details/tabs/milestone/feedback/cards/FeedbackCompletedCard.tsx
+++ src/flexternships/app/components/pages/project-details/tabs/milestone/feedback/cards/FeedbackCompletedCard.tsx
@@ -5,10 +5,14 @@ import { AlertCircle, Check } from 'react-feather';
 export default function FeedbackCompletedCard(props: FeedbackCompletedCardProps) {
   const { feedbackId, feedbackType, tiny = false } = props;
 
+  // using the var to avoid tsc error temporarily
+  console.log(feedbackId);
+
   return (
     <div
-      className={`flex flex-row justify-between items-center px-6 py-5 bg-success bg-opacity-[0.12] rounded-md ${tiny && 'w-6/12'
-        }`}
+      className={`flex flex-row justify-between items-center px-6 py-5 bg-success bg-opacity-[0.12] rounded-md ${
+        tiny && 'w-6/12'
+      }`}
     >
       <div className="flex flex-row items-center gap-x-3">
         {tiny ? (
@@ -27,8 +31,12 @@ export default function FeedbackCompletedCard(props: FeedbackCompletedCardProps)
           {feedbackCardContent.title[feedbackType]} Completed{tiny && '!'}
         </div>
       </div>
-      <div className='flex flex-row items-center gap-x-4'>
-        <span className={`${tiny ? 'text-sm text-success font-semibold' : 'text-base text-trublue-secondary-500 font-medium'} not-italic cursor-pointer`}>
+      <div className="flex flex-row items-center gap-x-4">
+        <span
+          className={`${
+            tiny ? 'text-sm text-success font-semibold' : 'text-base text-trublue-secondary-500 font-medium'
+          } not-italic cursor-pointer`}
+        >
           View
         </span>
         {tiny && (
