c11764252f4d08deaab8cd5fa56a26a3ced896d0
vikas2705 <vikas.vikasranjan@gmail.com>
2024-11-21 20:15:24 +0530

    comment notes

--- src/flexternships/app/components/pages/project-details/tabs/milestone/feedback/HandleFeedbacks.tsx
+++ src/flexternships/app/components/pages/project-details/tabs/milestone/feedback/HandleFeedbacks.tsx
@@ -21,10 +21,13 @@ export default function HandleFeedbacks() {
     return <Navigate to="/404" />;
   }
 
-  return (
-    <div className="w-full flex flex-row flex-wrap items-start gap-[10px] lg:gap-[26px]">
-      <div>{componentsByFeedbackType[feedbackType as keyof typeof componentsByFeedbackType]}</div>
-      <FunFacts />
-    </div>
-  );
+    return <div>{componentsByFeedbackType[feedbackType as keyof typeof componentsByFeedbackType]}</div>;
+
+
+  // return (
+  //   <div className="w-full flex flex-row flex-wrap items-start gap-[10px] lg:gap-[26px]">
+  //     <div>{componentsByFeedbackType[feedbackType as keyof typeof componentsByFeedbackType]}</div>
+  //     <FunFacts />
+  //   </div>
+  // );
 }
