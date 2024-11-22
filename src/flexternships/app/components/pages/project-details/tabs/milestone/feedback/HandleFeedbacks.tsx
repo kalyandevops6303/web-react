8e53837924de565a6a3fd42e5e5236929ab796d2
praneeth-rdy <praneeth@trumio.ai>
2024-11-22 19:27:36 +0530

    fix: removed an unused import

--- src/flexternships/app/components/pages/project-details/tabs/milestone/feedback/HandleFeedbacks.tsx
+++ src/flexternships/app/components/pages/project-details/tabs/milestone/feedback/HandleFeedbacks.tsx
@@ -4,7 +4,6 @@ import { MilestoneFeedbackType } from '@/flexternships/constraints/enums/core-en
 import TeamFeedback from './TeamFeedback';
 import PeerFeedback from './PeerFeedback';
 import IndividualFeedback from './IndividualFeedback';
-import FunFacts from './FunFacts';
 
 export default function HandleFeedbacks() {
   const componentsByFeedbackType = {
@@ -21,8 +20,7 @@ export default function HandleFeedbacks() {
     return <Navigate to="/404" />;
   }
 
-    return <div>{componentsByFeedbackType[feedbackType as keyof typeof componentsByFeedbackType]}</div>;
-
+  return <div>{componentsByFeedbackType[feedbackType as keyof typeof componentsByFeedbackType]}</div>;
 
   // return (
   //   <div className="w-full flex flex-row flex-wrap items-start gap-[10px] lg:gap-[26px]">
