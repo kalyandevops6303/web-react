233b0e82d9fce4f81579a4eb3bdf9fe240dc5b22
supratikch <supratik@trumio.ai>
2024-12-05 17:40:22 +0530

    blocked project milestones disabled + project tab filters fixed

--- src/flexternships/app/components/pages/project-details/tabs/milestone/DefaultMilestonesView.tsx
+++ src/flexternships/app/components/pages/project-details/tabs/milestone/DefaultMilestonesView.tsx
@@ -1,5 +1,7 @@
 import { MilestoneDetails } from '@/flexternships/constraints/types/project-milestones-types';
 import MilestoneTile from './MilestoneTile';
+import { useEffect, useState } from 'react';
+import { isEmpty } from 'lodash';
 
 type DefaultMilestonesViewProps = {
   milestones: MilestoneDetails[];
@@ -7,10 +9,17 @@ type DefaultMilestonesViewProps = {
 
 export default function DefaultMilestonesView(props: DefaultMilestonesViewProps) {
   const { milestones } = props;
+
+  const [isAnyMilestoneBlocked, setIsAnyMilestoneBlocked] = useState(false);
+
+  useEffect(() => {
+    !isEmpty(milestones) && setIsAnyMilestoneBlocked(milestones.some((milestone) => milestone.isBlocked));
+  }, [milestones]);
+
   return (
     <>
       {milestones.map((milestone, index) => (
-        <MilestoneTile data={milestone} key={index} />
+        <MilestoneTile data={milestone} key={index} disabled={isAnyMilestoneBlocked && !milestone?.isBlocked} />
       ))}
     </>
   );
