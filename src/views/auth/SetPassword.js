c8191cf4c8a105a967ff2be6392d2b9f2b975ceb
ShivamKumar2026 <shivam@trumio.ai>
2024-12-18 15:05:24 +0530

    Update SetPassword.js
    import lodash removed as it was not needed

--- src/views/auth/SetPassword.js
+++ src/views/auth/SetPassword.js
@@ -26,7 +26,6 @@ import { formData } from '../../redux/selectors/formDataSelectors';
 import { clearAllFormData, setFormData } from '../../redux/reducers/formData';
 import ShowToastMessage from '../../@core/components/toast';
 import { SUCCESS } from '../../utility/constants/ToastTypes';
-import { create } from 'lodash';
 
 const SetPassword = () => {
   const dispatch = useDispatch();
