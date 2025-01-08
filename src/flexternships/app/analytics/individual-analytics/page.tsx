011cef6e37c26383c315b6c65b61cae06e61d1cf
vikas2705 <vikas.vikasranjan@gmail.com>
2025-01-08 11:48:34 +0530

    remove extension

--- src/flexternships/app/analytics/individual-analytics/page.tsx
+++ src/flexternships/app/analytics/individual-analytics/page.tsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import IndividualOverview from './individual-overview';
-import MultipleLinesChart from '../../components/core/charts/MultipleLinesChart.tsx';
+import MultipleLinesChart from '../../components/core/charts/MultipleLinesChart';
 
 import { TooltipProps } from 'recharts';
 import { ArrowLeft, ThumbsUp } from 'react-feather';
