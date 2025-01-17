f804a7c40def84214235b1c5cc43034d62a2b41d
praneeth-rdy <praneeth@trumio.ai>
2025-01-17 20:00:59 +0530

    feat: added IDs and competencies store to performance insights

--- src/flexternships/app/analytics/team-analytics/performance-insights/page.tsx
+++ src/flexternships/app/analytics/team-analytics/performance-insights/page.tsx
@@ -1,8 +1,11 @@
 // 'use client'; // For NextJS App
 
 // React and hooks
-// import { useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+
+// State management
+import { useCompetenciesStore } from '@/flexternships/stores/competencies-store';
 
 // Icons
 import { ArrowLeft, Upload } from 'react-feather';
@@ -12,12 +15,12 @@ import CustomBreadCrumbs from '@/flexternships/app/components/core/CustomBreadCr
 import PrimaryIconText from '@/flexternships/app/components/core/buttons/PrimaryIconText';
 import SecondaryButton from '@/flexternships/app/components/core/buttons/SecondaryButton';
 import PerformanceInsightsCard from '@/flexternships/app/components/pages/analytics/team-analytics/performance-insights/PerformanceInsightsCard';
-
-// Mock data
-import { mockCompetencies } from '@/flexternships/mocks/competency-data';
+import Spinner from '@/flexternships/app/components/core/Spinner';
 
 export default function TeamPerformanceInsights() {
-  // const [isCompetenciesLoading, setIsCompetenciesLoading] = useState<boolean>(false);
+  const isCompetenciesLoading = useCompetenciesStore((state) => state.isCompetenciesLoading);
+  const competencies = useCompetenciesStore((state) => state.competencies);
+  const populateCompetencies = useCompetenciesStore((state) => state.populateCompetencies);
 
   const navigate = useNavigate();
   const { projectId } = useParams();
@@ -35,6 +38,10 @@ export default function TeamPerformanceInsights() {
     }
   };
 
+  useEffect(() => {
+    populateCompetencies();
+  }, [populateCompetencies]);
+
   return (
     <div className="flex flex-col gap-y-4">
       <div>
@@ -49,21 +56,29 @@ export default function TeamPerformanceInsights() {
       <div className="text-trublue-secondary-500">
         <PrimaryIconText icon={<ArrowLeft size={18} />} text="Team Analytics" onClick={goBack} />
       </div>
-      <div className="flex flex-col gap-y-4">
-        <div className="flex flex-row justify-between items-center">
-          <div className="text-grey-700 text-lg font-medium leading-[26px]">Team Performance Insights</div>
-          {/* TODO: Future export functionality */}
-          <SecondaryButton className="m-0 cursor-not-allowed" onClick={() => {}} disabled>
-            <Upload className="mr-2" size={18} />
-            Export
-          </SecondaryButton>
+      {isCompetenciesLoading ? (
+        <div className="flex flex-col items-center justify-center min-h-[60vh]">
+          <div className="size-10">
+            <Spinner />
+          </div>
         </div>
+      ) : (
         <div className="flex flex-col gap-y-4">
-          {mockCompetencies.map((competencyItem) => (
-            <PerformanceInsightsCard competencyItem={competencyItem} />
-          ))}
+          <div className="flex flex-row justify-between items-center">
+            <div className="text-grey-700 text-lg font-medium leading-[26px]">Team Performance Insights</div>
+            {/* TODO: Future export functionality */}
+            <SecondaryButton className="m-0 cursor-not-allowed" onClick={() => {}} disabled>
+              <Upload className="mr-2" size={18} />
+              Export
+            </SecondaryButton>
+          </div>
+          <div className="flex flex-col gap-y-4">
+            {competencies.map((competencyItem) => (
+              <PerformanceInsightsCard competencyItem={competencyItem} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
