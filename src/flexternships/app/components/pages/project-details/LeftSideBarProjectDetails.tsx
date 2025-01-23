2321175d26e22cbe1d4ed2b6f5e0eb7615827960
tr-vikas <vikas@trumio.ai>
2025-01-23 20:23:21 +0530

    fix modal close button

--- src/flexternships/app/components/pages/project-details/LeftSideBarProjectDetails.tsx
+++ src/flexternships/app/components/pages/project-details/LeftSideBarProjectDetails.tsx
@@ -56,7 +56,9 @@ const LeftSideBarProjectDetails = () => {
   const [showRelistModal, setShowRelistModal] = useState(false);
   const [tagsData, setTagsData] = useState<BadgeType[]>([]);
   const [showMore, setShowMore] = useState(false);
-  const modalRef = useRef<HTMLDivElement>(null);
+  // const modalRef = useRef<HTMLDivElement | null>(null);
+  const modalRef = useRef<HTMLDivElement>(null!) as React.RefObject<HTMLDivElement>;
+
   const handleToggle = () => {
     setShowMore((prev) => !prev);
   };
