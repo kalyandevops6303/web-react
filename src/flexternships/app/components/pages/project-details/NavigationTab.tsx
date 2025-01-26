73074c239f6c7de60974708ec8822e5bbdc31949
Soumyaraj Bag <soumyarajbag@gmail.com>
2025-01-27 00:03:44 +0530

    fix:classNames-issue

--- src/flexternships/app/components/pages/project-details/NavigationTab.tsx
+++ src/flexternships/app/components/pages/project-details/NavigationTab.tsx
@@ -7,24 +7,22 @@ import { Link, useParams } from 'react-router-dom';
 const NavigationTab = ({ tab, index, isActive }: { tab: ProjectTabType; index: number; isActive: boolean }) => {
   const userDetails = useFlexternUserStore((state) => state.userDetails);
   const param = useParams();
-  const activeTabClass = 'bg-[#0185E41F] text-trublue-secondary-500 border-b-2 border-b-trublue-secondary-500';
-  const inactiveTabClass = '';
-  const activeIconClass = 'bg-white text-trublue-secondary-500';
-  const inactiveIconClass = 'bg-[#9E9E9E1F] text-grey-loadingText';
 
   return (
     <Link
       to={`/project-details/${param?.projectId}/${tab.id}`}
       key={index}
       className={classNames(
-        isActive ? activeTabClass : inactiveTabClass,
+        {
+          'bg-[#0185E41F] text-trublue-secondary-500 border-b-2 border-b-trublue-secondary-500': isActive,
+          hidden: !(userDetails?.userType === userTypes?.client ? tab.clientVisible : tab.talentVisible),
+        },
         'group duration-200 ease-in-out hover:cursor-pointer hover:text-trublue-secondary-500 first:rounded-tl last:rounded-tr flex flex-row w-fit p-3 items-start justify-start gap-2',
-        !(userDetails?.userType === userTypes?.client ? tab.clientVisible : tab.talentVisible) && 'hidden',
       )}
     >
       <div
         className={classNames(
-          isActive ? activeIconClass : inactiveIconClass,
+          { 'bg-white text-trublue-secondary-500': isActive, 'bg-[#9E9E9E1F] text-grey-loadingText': !isActive },
           'h-[38px] w-[38px] flex flex-col justify-center items-center border rounded-[5px] group-hover:bg-white group-hover:text-trublue-secondary-500 transition-all ease-in-out',
         )}
       >
@@ -33,7 +31,7 @@ const NavigationTab = ({ tab, index, isActive }: { tab: ProjectTabType; index: n
       <div className="flex flex-col items-start justify-center">
         <h1
           className={classNames(
-            isActive ? 'text-trublue-secondary-500' : 'text-grey-800',
+            { 'text-trublue-secondary-500': isActive, 'text-grey-800': !isActive },
             'text-sm group-hover:text-trublue-secondary-500 font-semibold',
           )}
         >
@@ -41,7 +39,7 @@ const NavigationTab = ({ tab, index, isActive }: { tab: ProjectTabType; index: n
         </h1>
         <h1
           className={classNames(
-            isActive ? 'text-trublue-secondary-500' : 'text-grey-loadingText',
+            { 'text-trublue-secondary-500': isActive, 'text-grey-800': !isActive },
             'font-light group-hover:text-trublue-secondary-500 text-xs',
           )}
         >
