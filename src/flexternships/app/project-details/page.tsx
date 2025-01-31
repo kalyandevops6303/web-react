030d4136075f616e2ed2562da6c52408b25df5de
Soumyaraj Bag <soumyarajbag@gmail.com>
2025-01-31 16:24:05 +0530

    fix:breadcrumbs

--- src/flexternships/app/project-details/page.tsx
+++ src/flexternships/app/project-details/page.tsx
@@ -17,6 +17,7 @@ export default function FlexternshipProjectDetails() {
   const getProjectDetails = useProjectsStore((state) => state.getProjectDetails);
   const projectDetailsLoading = useProjectsStore((state) => state.projectDetailsLoading);
   const milestoneDetails = useProjectMilestonesStore((state) => state.milestoneDetails);
+  const milestoneDetailsLoading = useProjectMilestonesStore((state) => state.isMilestoneDetailsLoading);
   const projectDetails = useProjectsStore((state) => state.projectDetails);
   const projectLoading = useProjectsStore((state) => state.isProjectsLoading);
 
@@ -121,10 +122,13 @@ export default function FlexternshipProjectDetails() {
     if (projectDetails?.details?.name) {
       setBreadCrumbData((prev) => ({ ...prev, projectName: projectDetails?.details?.name }));
     }
-    if (params?.milestoneId) {
+    if (milestoneDetailsLoading) {
+      setBreadCrumbData((prev) => ({ ...prev, milestoneName: '' }));
+    }
+    if (!milestoneDetailsLoading && params?.milestoneId) {
       setBreadCrumbData((prev) => ({ ...prev, projectStep: 'Milestone', milestoneName: milestoneDetails?.name }));
     }
-  }, [params.projectStep, params?.milestoneId, projectDetails]);
+  }, [params.projectStep, params?.milestoneId, projectDetails, milestoneDetails, milestoneDetailsLoading]);
   const breadCrumbs = [
     {
       label: 'Projects',
