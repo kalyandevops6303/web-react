730729e289bf712aec4ba513e57b039e93bf520d
Soumyaraj Bag <soumyaraj@trumio.ai>
2025-02-03 13:59:43 +0530

    fix:desc-modal

--- src/flexternships/app/components/core/modals/ProjectDescriptionModal.tsx
+++ src/flexternships/app/components/core/modals/ProjectDescriptionModal.tsx
@@ -1,8 +1,8 @@
 'use client';
-import CloseModalButton from '../buttons/CloseModalButton';
+import GenericModal from './GenericModal';
 
 export default function ProjectDescriptionModal(props: ProjectDescriptionModalProps) {
-  const { isOpen, onClose, data, modalRef } = props;
+  const { isOpen, onClose, data } = props;
 
   if (!isOpen) {
     return null;
@@ -13,20 +13,12 @@ export default function ProjectDescriptionModal(props: ProjectDescriptionModalPr
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-20">
-      <div
-        className="relative w-full min-w-[40rem] max-w-sm rounded-lg bg-white shadow-lg  max-h-[80vh] "
-        ref={modalRef}
-      >
-        <CloseModalButton onClick={handleClose} />
-        <h1 className="text-grey-heading text-xl font-medium leading-[28px] pt-5 pr-8 pb-3 pl-6">
-          Project Description
-        </h1>
-        <div className="overflow-y-auto px-6  text-sm text-gray-800 max-h-[70vh]">
-          <p className="w-full max-w-full break-words pb-6">{data}</p>
-        </div>
+    <GenericModal className="pb-5 max-h-[70vh]" isOpen={isOpen} onClose={handleClose}>
+      <h1 className="text-grey-heading text-xl font-medium leading-[28px] pt-5 pr-8 pb-3 pl-6">Project Description</h1>
+      <div className="overflow-y-auto px-6 text-sm text-gray-800 max-h-[60vh]">
+        <p className="w-full max-w-full break-words pb-10">{data}</p>
       </div>
-    </div>
+    </GenericModal>
   );
 }
 
