dee986f89f02e1426ce6324796cb71637b37770b
saharoysoham <soham.saharoy@trumio.ai>
2025-02-12 11:42:32 +0530

    fix: TRU-2696 disabled login message in single line

--- src/views/auth/UserRetryCountAuth.js
+++ src/views/auth/UserRetryCountAuth.js
@@ -35,13 +35,11 @@ const UserRetryCountAuth = () => {
 
     return (
       <div className="text-error text-xs flex  relative mt-2" role="alert">
-        <p>Your account is disabled.</p>
         <p>
-          Please try again in{' '}
+          Your account is disabled. Please try again in{' '}
           <span className="font-semibold">
             {minutes}:{seconds.toString().padStart(2, '0')}
           </span>{' '}
-          {''}
           mins.
         </p>
       </div>
