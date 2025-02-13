7405304d6c9c952e2c9de255dc70f68aa9674b6b
praneeth-rdy <praneeth@trumio.ai>
2025-02-13 14:46:56 +0530

    fix: ux audit client avatar clicks

--- src/flexternships/app/project-details/tabs/projects/InvitationCard.tsx
+++ src/flexternships/app/project-details/tabs/projects/InvitationCard.tsx
@@ -9,6 +9,7 @@ import Styles from '@flexternships/styles/pages/project-details/projects-tab/tab
 import { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import FlexternAvatar from '@/flexternships/app/components/core/avatars/FlexternAvatar';
+import routes from '@/flexternships/routes';
 
 export default function InvitationCard({ hideSubtitle = false, isCollapsible = true }) {
   const params = useParams();
@@ -54,7 +55,8 @@ export default function InvitationCard({ hideSubtitle = false, isCollapsible = t
   };
 
   const invitationCardDetailsData = {
-    company: `${projectDetails?.clientInfo?.firstName} ${projectDetails?.clientInfo?.lastName}`,
+    clientUserId: projectDetails?.clientInfo?.userId,
+    clientName: `${projectDetails?.clientInfo?.firstName} ${projectDetails?.clientInfo?.lastName}`,
     department: projectDetails?.clientInfo?.departmentName,
     image_uri: projectDetails?.clientInfo?.imageUri,
     start_date: formatEpochToHumanReadable(projectInvitationDetails?.projectStartDate || 1),
@@ -83,13 +85,18 @@ export default function InvitationCard({ hideSubtitle = false, isCollapsible = t
     <CollapsableCard {...invitationCardData}>
       <div className="px-4 pt-4 flex flex-col gap-y-6 w-full">
         <div className="flex flex-row items-start w-full justify-between">
-          <div className="flex gap-2">
-            <FlexternAvatar name={invitationCardDetailsData?.company} imageUri={invitationCardDetailsData?.image_uri} />
-            <div>
-              <div className={Styles.invitationCardDetailsTitle}>{invitationCardDetailsData?.company}</div>
-              <div className={Styles.invitationCardDetailsSubtitle}>{invitationCardDetailsData?.department}</div>
+          <Link to={routes.clientProfile.generate(invitationCardDetailsData?.clientUserId)}>
+            <div className="flex gap-2">
+              <FlexternAvatar
+                name={invitationCardDetailsData?.clientName}
+                imageUri={invitationCardDetailsData?.image_uri}
+              />
+              <div>
+                <div className={Styles.invitationCardDetailsTitle}>{invitationCardDetailsData?.clientName}</div>
+                <div className={Styles.invitationCardDetailsSubtitle}>{invitationCardDetailsData?.department}</div>
+              </div>
             </div>
-          </div>
+          </Link>
           <div className="min-w-20">
             <h1 className="text-xs">{timeGapOfInvite} Day(s) ago</h1>
           </div>
