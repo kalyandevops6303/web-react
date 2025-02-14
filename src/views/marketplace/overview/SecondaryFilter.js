36cc10fd99eefb2831edb9e583993725632c05b0
praneeth-rdy <praneeth@trumio.ai>
2025-02-14 19:13:13 +0530

    fix: department name filter for all listings on marketplace

--- src/views/marketplace/overview/SecondaryFilter.js
+++ src/views/marketplace/overview/SecondaryFilter.js
@@ -329,7 +329,7 @@ const SecondaryFilters = ({ primaryFilter, userType, appRole }) => {
       };
     }
     try {
-      const response = await departmentNamesService({ projectStatus: 'OPEN' });
+      const response = await departmentNamesService({ projectStatus: primaryFilter === 'all_listings' ? '' : 'OPEN' });
       const options = response?.data?.data?.data?.map((department) => ({
         label: department.department_name,
         value: department.department_name,
