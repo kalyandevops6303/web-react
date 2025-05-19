64cc65069fd9f54e7c5f7869c8dd9ed075d4a433
praneeth-rdy <praneeth@trumio.ai>
2025-05-19 12:28:43 +0530

    fix: TRU-3931

--- src/flexternships/utils/text-utils.ts
+++ src/flexternships/utils/text-utils.ts
@@ -10,3 +10,16 @@ export const toTitleCase = (text: string): string => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
 };
+
+/**
+ * Masks the email address by replacing the middle characters with asterisks.
+ * @param email - The email address to be masked.
+ * @returns The masked email address.
+ */
+export const getMaskedEmail = (email: string): string => {
+  if (!email) return '';
+  const [localPart, domain] = email.split('@');
+  if (localPart.length <= 2) return email;
+  const maskedLocalPart = localPart.slice(0, 2) + '****' + localPart.slice(-1);
+  return `${maskedLocalPart}@${domain}`;
+};
