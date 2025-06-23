a2f67aa6523838d806db5a6f808559d9dadd2864
Soumyaraj Bag <soumyaraj@trumio.ai>
2025-06-23 22:46:03 +0530

    fix:create-project-error

--- src/flexternships/app/components/pages/create-project/tabs/preview/RoleItem.tsx
+++ src/flexternships/app/components/pages/create-project/tabs/preview/RoleItem.tsx
@@ -6,17 +6,13 @@ export default function RoleItem(props: Props) {
   const { last = false, data } = props;
   return (
     <div className={`${Styles.roleItem} ${last ? '' : 'border-b-1'}`}>
-      <div className={Styles.role}>{data.role?.name || '(Add role)'}</div>
-      <div className={Styles.count}>{data.count || '(Add count)'}</div>
+      <div className={Styles.role}>{String(data.role?.name || '(Add role)')}</div>
+      <div className={Styles.count}>{String(data.count || '(Add count)')}</div>
       <div className={Styles.skills}>
-        {data.skills.map((skill, index) => (
-          <PrimaryTag key={index} content={skill.name} />
-        ))}
+        {data.skills?.map((skill, index) => <PrimaryTag key={index} content={String(skill?.name || 'Skill')} />) || []}
       </div>
       <div className={Styles.tools}>
-        {data.tools.map((tool, index) => (
-          <PrimaryTag key={index} content={tool.name} />
-        ))}
+        {data.tools?.map((tool, index) => <PrimaryTag key={index} content={String(tool?.name || 'Tool')} />) || []}
       </div>
     </div>
   );
