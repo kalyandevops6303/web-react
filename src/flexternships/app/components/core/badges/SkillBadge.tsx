be081f77d72f3536dfbd43afdf33ab5da3a2a124
praneeth-rdy <praneeth@trumio.ai>
2025-06-25 09:55:18 +0530

    refactor: mocks and enum

--- src/flexternships/app/components/core/badges/SkillBadge.tsx
+++ src/flexternships/app/components/core/badges/SkillBadge.tsx
@@ -1,9 +1,10 @@
 import { cn } from '@/lib/utils';
 import classNames from 'classnames';
+import { SkillBadgeType } from '@/flexternships/constraints/enums/miscellaneous-enums';
 
 interface SkillBadgeProps {
   name: string;
-  type: 'cohort' | 'project';
+  type: SkillBadgeType;
   className?: string;
 }
 
@@ -12,8 +13,8 @@ export default function SkillBadge({ name, type, className }: SkillBadgeProps) {
     <span
       className={cn(
         classNames('rounded-[4px] px-[8px] py-[1px] text-xs font-semibold leading-4.5', {
-          'bg-trublue-light text-trublue-secondary-500': type === 'cohort',
-          'bg-[#FFB7011F] text-yellow-secondary-500': type === 'project',
+          'bg-trublue-light text-trublue-secondary-500': type === SkillBadgeType.COHORT,
+          'bg-[#FFB7011F] text-yellow-secondary-500': type === SkillBadgeType.PROJECT,
         }),
         className,
       )}
